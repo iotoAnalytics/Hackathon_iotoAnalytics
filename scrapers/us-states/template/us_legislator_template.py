6fa06414982e082dcea629d1ea0748e85da8bf84
Justin Tendeck <justin.tendeck@gmail.com>
2021-01-13 15:57:02 -0800

    Added template files and utils

---
+++ scrapers/us-states/template/us_legislator_template.py
@@ -0,0 +1,95 @@
+'''
+Before beginning, be sure to update values in the config file.
+
+This template is meant to serve as a general outline, and will not necessarily work for
+all pages. Feel free to modify the scripts as necessary.
+
+Note that the functions in the scraper_utils.py and database_tables.py file should not
+have to change. Please extend the classes in these files if you need to modify them.
+'''
+import sys, os
+from pathlib import Path
+
+# Get path to the root directory so we can import necessary modules
+p = Path(os.path.abspath(__file__)).parents[3]
+
+sys.path.insert(0, str(p))
+
+from scraper_utils import ScraperUtils
+from bs4 import BeautifulSoup
+import requests
+from multiprocessing import Pool
+import configparser
+
+# Initialize config parser and get variables from config file
+configParser = configparser.RawConfigParser()
+configParser.read('config.cfg')
+
+state_abbreviation = str(configParser.get('scraperConfig', 'state_abbreviation'))
+database_table_name = str(configParser.get('scraperConfig', 'database_table_name'))
+country = str(configParser.get('scraperConfig', 'country'))
+
+
+def get_urls():
+    '''
+    Insert logic here to get all URLs you will need to scrape from the page.
+    '''
+    urls = []
+
+    # Logic goes here! Some sample code:
+    base_url = 'https://webscraper.io'
+    path = '/test-sites/e-commerce/allinone'
+    scrape_url = base_url + path
+    page = requests.get(scrape_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    urls = [base_url + prod_path['href'] for prod_path in soup.findAll('a', {'class': 'title'})]
+    
+    return urls
+
+
+def scrape(url):
+    '''
+    Insert logic here to scrape all URLs acquired in the get_urls() function.
+
+    Do not worry about collecting the goverlytics_id, date_collected, country, country_id,
+    state, and state_id values, as these have already been inserted by the initialize_row()
+    function, or will be inserted when placed in the database.
+
+    Do not worry about trying to insert missing fields as the initialize_row function will
+    insert empty values for us.
+
+    Be sure to insert the correct data type into each row. Refer to the data dictionary if
+    unsure.
+    '''
+    
+    row = scraper_utils.initialize_row()
+
+    # Now you can begin collecting data and fill in the row. The row is a dictionary where the
+    # keys are the columns in the data dictionary. For instance, we can insert the state_url,
+    # like so:
+    row['state_url'] = url
+
+    # The only thing to be wary of is collecting the party and party_id. You'll first have to collect
+    # the party name from the website, then get the party_id from the parties data frame.
+    # This can be done like so:
+    party = 'Republican' # Insert logic to collect party for legislator. Must be full party name. Ie: Democrat, Republican, etc.
+    row['party_id'] = scraper_utils.get_party_id(party) # Get party_id using scraper_utils
+    row['party'] = party
+
+    # Other than that, you can replace this statement with the rest of your scraper logic.
+
+    return row
+
+if __name__ == '__main__':
+    scraper_utils = ScraperUtils(state_abbreviation, database_table_name, country)
+
+    urls = get_urls()
+
+    # Here we can use Pool from the multiprocessing library to speed things up.
+    # We can iterate through the URLs individually, which is slower:
+    data = [scrape(url) for url in urls]
+
+    # with Pool() as pool:
+    #     data = pool.map(scrape, urls)
+
+    scraper_utils.insert_legislator_data_into_db(data)
