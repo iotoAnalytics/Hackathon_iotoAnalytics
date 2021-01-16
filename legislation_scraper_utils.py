a28dc6c88cd8dc72269211b29f9658a714c719ac
Justin Tendeck <justin.tendeck@gmail.com>
2021-01-15 17:29:14 -0800

    Started work on legislation scraper

--- legislation_scraper_utils.py
+++ legislation_scraper_utils.py
@@ -6,46 +6,61 @@ import json
 import sys
 from database_tables import DatabaseTables
 from database import CursorFromConnectionFromPool
+import pandas as pd
+from pandas.core.computation.ops import UndefinedVariableError
 
 
 columns = [
-    'state_member_id',
-    'most_recent_term_id',
+    'goverlytics_id',
+    'bill_state_id',
+    'bill_name',
+    'session',
+    'date_introduced',
     'state_url',
     'url',
-    'name_full',
-    'name_last',
-    'name_first',
-    'name_middle',
-    'name_suffix',
-    'country_id',
-    'country',
+    'chamber_origin',
+    'committees',
     'state_id',
     'state',
-    'party_id',
-    'party',
-    'role',
-    'district',
-    'years_active',
-    'committees',
-    'areas_served',
-    'phone_number',
-    'addresses',
-    'email',
-    'birthday',
-    'seniority',
-    'occupation',
-    'education',
-    'military_experience'
+    'bill_type',
+    'bill_title',
+    'current_status',
+    'principal_sponsor_id',
+    'principal_sponsor',
+    'sponsors',
+    'sponsors_id',
+    'cosponsors',
+    'cosponsors_id',
+    'bill_text',
+    'bill_description',
+    'bill_summary',
+    'actions',
+    'votes',
+    'topic'
 ]
 
 
-class LegislatorScraperUtils:
-    def __init__(self, state_abbreviation, database_table_name, country):
+class LegislationScraperUtils:
+    def __init__(self, state_abbreviation, database_table_name, legislator_table_name):
         self.state_abbreviation = state_abbreviation
         self.database_table_name = database_table_name
-        self.country = country
-        self.db_tables = DatabaseTables()
+        self.legislator_table_name = legislator_table_name
+        
+        with CursorFromConnectionFromPool() as curs:
+            try:
+                query = 'SELECT state_no, state_name, abbreviation FROM us_state_info'
+                curs.execute(query)
+                state_results = curs.fetchall()
+
+                query = f'SELECT * FROM {legislator_table_name}'
+                curs.execute(query)
+                legislator_results = curs.fetchall()
+            except Exception as e:
+                sys.exit(f'An exception occurred retrieving either US parties or state legislator table from database. \
+                \nHas the legislator data been collected for this state yet?\n{e}')
+
+        self.states = pd.DataFrame(state_results)
+        self.legislators = pd.DataFrame(legislator_results)
 
     
     def __json_serial(self, obj):
@@ -64,38 +79,24 @@ class LegislatorScraperUtils:
         row = {column_name: '' for column_name in columns}
         
         try:
-            row['country'] = self.country
-            row['country_id'] = int(self.db_tables.countries.loc[self.db_tables.countries['country'] == self.country]['id'].values[0])
             row['state'] = self.state_abbreviation
-            row['state_id'] = int(self.db_tables.states.loc[self.db_tables.states['abbreviation'] == self.state_abbreviation]['state_no'].values[0])
+            row['state_id'] = int(self.states.loc[self.states['abbreviation'] == self.state_abbreviation]['state_no'].values[0])
         except IndexError:
-            sys.exit('An error occurred inserting state_id and/or country_id. Did you update the config file?')
+            sys.exit('An error occurred inserting state_id. Has the config file been updated?')
         except Exception as e:
             sys.exit(f'An error occurred involving the state_id and/or country_id: {e}')
         
-        row['years_active'] = []
+        row['date_introduced'] = None
         row['committees'] = {}
-        row['areas_served'] = []
-        row['phone_number'] = {}
-        row['seniority'] = 0
-        row['occupation'] = []
-        row['education'] = {}
-        row['addresses'] = []
-        row['birthday'] = None
+        row['principal_sponsor_id'] = 0
+        row['sponsors'] = []
+        row['sponsors_id'] = []
+        row['cosponsors'] = []
+        row['cosponsors_id'] = []
+        row['actions'] = {}
+        row['votes'] = {}
 
         return row
-
-    
-    def get_party_id(self, party_name : str):
-        try:
-            party_id = int(self.db_tables.parties.loc[self.db_tables.parties['party'] == party_name]['id'].values[0])
-        except IndexError:
-            sys.exit('An error occurred getting party_id.\nParty not found. Has the party been collected, and is it in the correct format?')
-        except Exception as e:
-            sys.exit(f'An error occurred involving the party_id: {e}')
-
-        return party_id
-
     
     def insert_legislator_data_into_db(self, data : list):
 
@@ -103,36 +104,34 @@ class LegislatorScraperUtils:
             try:
                 create_table_query = sql.SQL("""
                     CREATE TABLE IF NOT EXISTS {table} (
-                        goverlytics_id bigint PRIMARY KEY,
-                        state_member_id text,
-                        most_recent_term_id text,
+                        goverlytics_id text PRIMARY KEY,
+                        bill_state_id text,
                         date_collected timestamp,
-                        state_url TEXT UNIQUE,
+                        bill_name text,
+                        session TEXT,
+                        date_introduced date,
+                        state_url text UNIQUE,
                         url text,
-                        name_full text,
-                        name_last text,
-                        name_first text,
-                        name_middle text,
-                        name_suffix text,
-                        country_id bigint,
-                        country text,
+                        chamber_origin text,
+                        committees jsonb,
                         state_id int,
                         state char(2),
-                        party_id int,
-                        party text,
-                        role text,
-                        district text,
-                        years_active int[],
-                        committees jsonb,
-                        areas_served text[],
-                        phone_number jsonb,
-                        addresses jsonb,
-                        email text,
-                        birthday date,
-                        seniority int,
-                        occupation text[],
-                        education jsonb,
-                        military_experience text
+                        bill_type text,
+                        bill_title text,
+                        current_status text,
+                        principal_sponsor_id int,
+                        principal_sponsor text,
+                        sponsors text[],
+                        sponsors_id int[],
+                        cosponsors text[],
+                        cosponsors_id int[],
+                        bill_text text,
+                        bill_description text,
+                        bill_summary text,
+                        actions jsonb,
+                        votes jsonb,
+                        site_topic text,
+                        topic text
                     );
                     """).format(table=sql.Identifier(self.database_table_name))
 
@@ -142,62 +141,111 @@ class LegislatorScraperUtils:
                 print(f'An exception occurred creating {self.database_table_name}:\n{e}')
 
             insert_legislator_query = sql.SQL("""
-                WITH leg_id AS (SELECT NEXTVAL('legislator_id') leg_id)
                 INSERT INTO {table}
                 VALUES (
-                    (SELECT leg_id FROM leg_id), %s, %s, %s, %s,
-                    CONCAT('us/{state}/legislators/', (SELECT leg_id FROM leg_id)),
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-                    %s, %s, %s, %s, %s, %s, %s)
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 ON CONFLICT (state_url) DO UPDATE SET
                     date_collected = excluded.date_collected,
-                    name_full = excluded.name_full,
-                    name_last = excluded.name_last,
-                    name_first = excluded.name_first,
-                    name_middle = excluded.name_middle,
-                    name_suffix = excluded.name_suffix,
-                    country_id = excluded.country_id,
-                    country = excluded.country,
-                    state_id = excluded.state_id,
+                    bill_title = excluded.bill_title,
+                    bill_name = excluded.bill_name,
+                    bill_type = excluded.bill_type,
+                    sponsors = excluded.sponsors,
+                    sponsors_id = excluded.sponsors_id,
+                    principal_sponsor_id = excluded.principal_sponsor_id,
+                    principal_sponsor = excluded.principal_sponsor,
+                    current_status = excluded.current_status,
+                    actions = excluded.actions,
+                    date_introduced = excluded.date_introduced,
+                    chamber_origin = excluded.chamber_origin,
+                    session = excluded.session,
                     state = excluded.state,
-                    party_id = excluded.party_id,
-                    party = excluded.party,
-                    district = excluded.district,
-                    role = excluded.role,
+                    state_id = excluded.state_id,
+                    site_topic = excluded.site_topic,
+                    votes = excluded.votes,
+                    goverlytics_id = excluded.goverlytics_id,
+                    url = excluded.url,
+                    bill_state_id = excluded.bill_state_id,
                     committees = excluded.committees,
-                    areas_served = excluded.areas_served,
-                    phone_number = excluded.phone_number,
-                    addresses = excluded.addresses,
-                    email = excluded.email,
-                    birthday = excluded.birthday,
-                    military_experience = excluded.military_experience,
-                    occupation = excluded.occupation,
-                    education = excluded.education,
-                    state_member_id = excluded.state_member_id,
-                    most_recent_term_id = excluded.most_recent_term_id,
-                    years_active = excluded.years_active,
-                    seniority = excluded.seniority;
+                    cosponsors = excluded.sponsors,
+                    cosponsors_id = excluded.cosponsors_id,
+                    topic = excluded.topic,
+                    bill_text = excluded.bill_text,
+                    bill_description = excluded.bill_description,
+                    bill_summary = excluded.bill_summary;
                 """).format(table=sql.Identifier(self.database_table_name), state=sql.SQL(self.state_abbreviation))
 
             date_collected = datetime.now()
 
             for row in data:
                 try:
-                    tup = (row['state_member_id'], row['most_recent_term_id'], date_collected, row['state_url'],
-                    row['name_full'], row['name_last'], row['name_first'], row['name_middle'], row['name_suffix'],
-                    row['country_id'], row['country'], row['state_id'], row['state'], row['party_id'], row['party'],
-                    row['role'], row['district'], row['years_active'],
-                    json.dumps(row['committees'], default=LegislatorScraperUtils.__json_serial),
-                    row['areas_served'],
-                    json.dumps(row['phone_number'], default=LegislatorScraperUtils.__json_serial),
-                    json.dumps(row['addresses'], default=LegislatorScraperUtils.__json_serial),
-                    row['email'], row['birthday'], row['seniority'],
-                    row['occupation'],
-                    json.dumps(row['education'], default=LegislatorScraperUtils.__json_serial),
-                    row['military_experience'])
+                    tup = (row['goverlytics_id'], row['bill_state_id'], date_collected, row['bill_name'],
+                    row['session'], row['date_introduced'], row['state_url'], row['url'], row['chamber_origin'],
+                    json.dumps(row['committees'], default=LegislationScraperUtils.__json_serial),
+                    row['state_id'], row['state'], row['bill_type'], row['bill_title'], row['current_status'],
+                    row['principal_sponsor_id'], row['principal_sponsor'], row['sponsors'], row['sponsors_id'],
+                    row['cosponsors'], row['cosponsors_id'], row['bill_text'], row['bill_description'], row['bill_summary'],
+                    json.dumps(row['actions'], default=LegislationScraperUtils.__json_serial),
+                    json.dumps(row['votes'], default=LegislationScraperUtils.__json_serial),
+                    row['site_topic'], row['topic'])
 
                     # print(f'Inserting <Row {row["state_url"]}>')
                     curs.execute(insert_legislator_query, tup)
 
                 except Exception as e:
                     print(f'An exception occurred inserting <Row {row["state_url"]}>: {e}')
+
+    def search_for_legislators(self, **kwargs):
+        query_lst = []
+        for k, v in kwargs.items():
+            q = ''
+            if isinstance(v, int):
+                q = f'{k}=={v}'
+            elif isinstance(v, str):
+                q = f'{k}=="{v}"'
+            else:
+                print(f'Unable to use {k}: {v} as search parameter. Must search by either a text or int column.')
+                continue
+            query_lst.append(q)
+
+        query = ' & '.join(query_lst)
+        try:
+            df = self.legislators.query(query)
+        except UndefinedVariableError as e:
+            print(f'Column not found: {e}')
+            return None
+        except Exception as e:
+            print(f'An error occurred finding legislator: {e}')
+            return None
+
+        if len(df) > 1:
+            print(f'WARNING: More than one legislator found using {kwargs} search parameter! \
+            Must use a more unique identifier!')
+            return None
+        if len(df) == 0:
+            print(f'WARNING: No legislators found while searching {kwargs}!')
+            return None
+
+        return df
+
+    def get_legislator_id(self, **kwargs):
+        df = self.search_for_legislators(**kwargs)
+        if df is not None:
+            return df.iloc[0]['goverlytics_id']
+        else:
+            return None
+
+    def get_legislator_id_list(self, search_list):
+        legislator_ids = []
+        for item in search_list:
+            if not isinstance(item, dict):
+                print(f'Unable to find legislator ID for: {item}. Search parameters must be a dictionary!')
+            else:
+                legislator_ids.append(self.get_legislator_id(**item))
+        return legislator_ids
+
+    def get_first_name_using_initial(self, first_name_initial, **kwargs):
+        df = self.search_for_legislators(**kwargs)
+        if df is not None:
+            return df.loc[df["name_first"].str.startswith(first_name_initial)]["name_first"].values[0]
+        return 
---
+++ scrapers/us-states/template/legislation/us_legislation_template.py
@@ -0,0 +1,189 @@
+'''
+Before beginning, be sure to update values in the config file.
+
+This template is meant to serve as a general outline, and will not necessarily work for
+all pages. Feel free to modify the scripts as necessary.
+
+Note that the functions in the scraper_utils.py and database_tables.py file should not
+have to change. Please extend the classes in these files if you need to modify them.
+'''
+import sys, os
+from pathlib import Path
+
+# Get path to the root directory so we can import necessary modules
+p = Path(os.path.abspath(__file__)).parents[4]
+
+sys.path.insert(0, str(p))
+
+from legislation_scraper_utils import LegislationScraperUtils
+from bs4 import BeautifulSoup
+import requests
+from multiprocessing import Pool
+from database import Database
+import configparser
+from pprint import pprint
+from nameparser import HumanName
+import re
+
+# Initialize config parser and get variables from config file
+configParser = configparser.RawConfigParser()
+configParser.read('config.cfg')
+
+state_abbreviation = str(configParser.get('scraperConfig', 'state_abbreviation'))
+database_table_name = str(configParser.get('scraperConfig', 'database_table_name'))
+legislator_table_name = str(configParser.get('scraperConfig', 'legislator_table_name'))
+
+#Initialize database and scraper utils
+db_user = str(configParser.get('databaseConfig', 'db_user'))
+db_pass = str(configParser.get('databaseConfig', 'db_pass'))
+db_host = str(configParser.get('databaseConfig', 'db_host'))
+db_name = str(configParser.get('databaseConfig', 'db_name'))
+
+Database.initialise(database=db_name, host=db_host, user=db_user, password=db_pass)
+
+scraper_utils = LegislationScraperUtils(state_abbreviation, database_table_name, legislator_table_name)
+
+
+def get_urls():
+    '''
+    Insert logic here to get all URLs you will need to scrape from the page.
+    '''
+    urls = []
+
+    # Logic goes here! Some sample code:
+    base_url = 'https://webscraper.io'
+    path = '/test-sites/e-commerce/allinone'
+    scrape_url = base_url + path
+    page = requests.get(scrape_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    urls = [base_url + prod_path['href'] for prod_path in soup.findAll('a', {'class': 'title'})]
+    
+    return urls
+
+
+def scrape(url):
+    '''
+    Insert logic here to scrape all URLs acquired in the get_urls() function.
+
+    Do not worry about collecting the date_collected, state, and state_id values,
+    as these have already been inserted by the initialize_row()
+    function, or will be inserted when placed in the database.
+
+    Do not worry about trying to insert missing fields as the initialize_row function will
+    insert empty values for us.
+
+    Be sure to insert the correct data type into each row. Otherwise, you will get an error
+    when inserting data into database. Refer to the data dictionary to see data types for
+    each column.
+    '''
+    
+    row = scraper_utils.initialize_row()
+
+    # Now you can begin collecting data and fill in the row. The row is a dictionary where the
+    # keys are the columns in the data dictionary. For instance, we can insert the state_url,
+    # like so:
+    row['state_url'] = url
+
+    # Depending on the data you're able to collect, the legislation scraper may be more involved
+    # Than the legislator scraper. For one, you will need to create the goverlytics_id. The 
+    # goverlytics_id is composed of the state, session, and bill_name, The goverlytics_id can be
+    # created like so:
+    # goverlytics_id = f'{state_abbreviation}_{session}_{bill_name}'
+    # row['goverlytics_id'] = goverlytics_id
+
+    # Once you have the goverlytics_id, you can create the url:
+    # row['url'] = f'/us/{state_abbreviation}/legislation/{goverlytics_id}'
+
+    # The sponsor and cosponsor ID's are where things can get complicated, depending on how
+    # much and what kind of data the legislation page has on the (co)sponsors. The
+    # legislator_id's are pulled from the legislator database table, so you must be able to
+    # uniquely identify each (co)sponsor... using just a last name, for instance, is not
+    # sufficient since often more than one legislator will have the same last name. If you
+    # have a unique identifier such as the (co)sponsor's state_url or state_member_id, use
+    # that. Otherwise, you will have to use some combination of the data available to
+    # identify. Using a first and last name may be sufficient.
+
+    # To get the ids, first get the identifying fields, then pass them into the
+    # get_legislator_id() function:
+    # row['principal_sponsor_id'] = get_legislator_id(state_url=legislator_state_url)
+    # The get_legislator_id function takes in any number of arguments, where the key is
+    # the column in the legislator table you want to search, and the value is the value
+    # you want to search that column for. So having:
+    # name_first = 'Joe'
+    # name_last = 'Jimbo'
+    # row['principal_sponsor_id'] = get_legislator_id(name_first=name_first, name_last=name_last)
+    # Will search the legislator table for the legislator with the first and last name Joe Jimbo.
+    # Note that the value passed in must match exactly the value you are searching for, including
+    # case and diacritics.
+
+    # In the past, I've typically seen legislators with the same last name denoted with with their
+    # first name initial. Eg: A. Smith. If this is the case, you will need to find out what the A.
+    # in A. smith stands for. scraper_utils has a function that lets you search for a legislator
+    # based on their first initial along with other parameters you want to search by:
+    # fname_initial = 'A.'
+    # name_last = 'Smith'
+    # fname_initial = fname_initial.upper().replace('.', '') # Be sure to clean up the initial as necessary!
+    # You can also search by multiple letters, say 'Ja' if you were searching for 'Jason'
+    # name_first = scraper_utils.get_first_name_using_initial(fname_initial, name_last=name_last)
+
+    # Finally, you can get the list of (co)sponsor IDs using the get_legislator_id_list() function.
+    # This function takes in a list of dictionaries, where the dictionaries contain the data you 
+    # want to search by:
+    # search_for = [
+    #     {
+    #         'name_first': 'Joe', 'name_last': 'Jimbo',
+    #         'name_first': 'Billy', 'name_last': 'Williams', #etc...
+    #     }
+    # ]
+    # cosponsor_ids = scraper_utils.get_legislator_id_list(search_for)
+
+    # Other than that, you can replace this statement with the rest of your scraper logic.
+
+    return row
+
+if __name__ == '__main__':
+    # # First we'll get the URLs we wish to scrape:
+    # urls = get_urls()
+
+    # # Next, we'll scrape the data we want to collect from those URLs.
+    # # Here we can use Pool from the multiprocessing library to speed things up.
+    # # We can also iterate through the URLs individually, which is slower:
+    # # data = [scrape(url) for url in urls]
+    # with Pool() as pool:
+    #     data = pool.map(scrape, urls)
+
+    # # Once we collect the data, we'll write it to the database.
+    # scraper_utils.insert_legislator_data_into_db(data)
+
+    name_first = 'Patricia'
+    name_last = 'Van Pelt'
+
+    search = [
+        {
+            'name_first': 'Patricia',
+            'name_last': 'Van Pelt'
+        },
+        {
+            'name_first': 'Dale',
+            'name_last': 'Fowler'
+        },
+        {
+            'state_member_id': 2721
+        }
+    ]
+
+    # leg_id = scraper_utils.get_legislator_id(name_first=name_first, name_last=name_last)
+    # leg_id = scraper_utils.get_legislator_id(most_recent_term_id=None)
+    # leg_id = scraper_utils.get_legislator_id(butts='big')
+
+    # name_first = df.loc[df["name_first"].str.startswith(fname_initial) & df["name_last"] == lname]["name_first"].values[0]
+    
+    # print(leg_id)
+
+    # leg_ids = scraper_utils.get_legislator_ids(search)
+
+    name = scraper_utils.get_first_name_using_initial('Ja', name_last='Plummer')
+
+    print(name)
+
+    print('Complete!')
