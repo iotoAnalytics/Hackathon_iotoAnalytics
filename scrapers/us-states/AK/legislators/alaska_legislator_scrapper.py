413bf02d563e795aa4b8f730865ad1be9996ace4
Samuel <77296931+sleung124@users.noreply.github.com>
2021-03-12 14:38:55 -0800

    Fixed bugs regarding party_id and party parameters

--- scrapers/us-states/AK/legislators/alaska_legislator_scrapper.py
+++ scrapers/us-states/AK/legislators/alaska_legislator_scrapper.py
@@ -343,9 +343,13 @@ def scrape_gov(data_dict):
     row.committees = find_com(url)
     row.years_active = find_years(url)
 
-    row.party = data_dict['Party']
-    if data_dict['Party'] == 'Democrat' or data_dict['Party'] == 'Republican':
-        row.party_id = scraper_utils.get_party_id(data_dict['Party'])
+    if data_dict['Party'] == 'Non Affiliated':
+      row.party = 'No Affiliation'
+    else:
+      row.party = data_dict['Party']
+    
+    row.party_id = scraper_utils.get_party_id(data_dict['Party'])
+                 
     # wiki info
     get_wiki_links(data_dict)
     if data_dict['wiki'] is not None:
