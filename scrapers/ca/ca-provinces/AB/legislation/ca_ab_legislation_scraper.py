40b485d3919625276465fec6830a113483fba8e1
anvo2 <avossen@ioto.ca>
2021-04-07 13:36:15 -0700

    AB Legislation complete

--- scrapers/ca/ca-provinces/AB/legislation/ca_ab_legislation_scraper.py
+++ scrapers/ca/ca-provinces/AB/legislation/ca_ab_legislation_scraper.py
@@ -65,17 +65,139 @@ def scrape_bill_links(link):
 
 
 def scrape_bills(link):
-    print(link)
+
     row = scraper_utils.initialize_row()
     row.source_url = link
     row.chamber_origin = 'Legislative Assembly'
     bill_id = link.split("infoid=")[1]
     bill_id = bill_id.split('&')[0]
-    print(bill_id)
+
     row.source_id = bill_id
-    # get goverlytics_id, url
-    row.goverlytics_id = "QC_2018_" + bill_id
-    # url = "/ca/QC/legislation/" + goverlytics_id
+
+    uClient = uReq(link)
+    page_html = uClient.read()
+    uClient.close()
+    # # html parsing
+    page_soup = soup(page_html, "html.parser")
+
+    details = page_soup.findAll("div", {"class": "detail"})
+    bill_name_details = (details[0]).findAll("div")
+    bill_name = bill_name_details[1].text
+    row.bill_name = bill_name.replace(u'\xa0', "")
+
+    bill_title_details = (details[1].findAll("div"))
+    bill_title = bill_title_details[1].text
+    row.bill_title = bill_title
+
+    session_details = (details[2]).findAll("div")
+    session = session_details[1].text
+    row.session = session.split(",")[1].strip()
+
+    row.goverlytics_id = "AB_" + row.session.replace(" ", "") + '_' + row.bill_name
+
+    sponsor_details = (details[3]).findAll("div")
+    prin_sponsor = sponsor_details[1].text
+    row.principal_sponsor = prin_sponsor
+
+    search_for = dict(name_last=prin_sponsor)
+    try:
+
+        s_id = scraper_utils.get_legislator_id(**search_for)
+        s_id = int(s_id)
+
+        principal_sponsor_id = (s_id)
+
+    except:
+
+        principal_sponsor_id = 0
+    row.principal_sponsor_id = principal_sponsor_id
+
+    type_details = (details[4]).findAll("div")
+    bill_type = type_details[1].text
+    row.bill_type = bill_type
+
+    pdf_details = (details[7]).findAll("div")
+    # print(pdf_details)
+
+    bill_pdf = pdf_details[1].a["href"]
+    # print(bill_pdf)
+    bill_text = ""
+
+    try:
+        r = requests.get(bill_pdf)
+        f = io.BytesIO(r.content)
+        reader = PyPDF2.PdfFileReader(f, strict=False)
+        if reader.isEncrypted:
+            reader.decrypt('')
+
+        contents = reader.getPage(0).extractText()
+        bill_text = contents
+        bill_text = bill_text.replace("\n", "")
+
+    except:
+
+        pass
+    row.bill_text = bill_text
+
+
+    bill_entries = page_soup.find("div", {"class": "bill-entries"})
+    first = page_soup.find("div", {"class": "b_entry"})
+    date_introduced = first.div.div.span.text
+    date_introduced = date_introduced.split(" ")
+    date_introduced = " ".join(date_introduced[:3])
+    # print(date_introduced)
+    d = datetime.datetime.strptime(date_introduced, '%b %d, %Y')
+    d = d.strftime('%Y-%m-%d')
+    row.date_introduced = d
+
+    bill_entries = bill_entries.findAll("div")
+    actions = []
+    action_info = page_soup.findAll(True, {'class': ['b_header', 'b_entry']})
+    for a in action_info:
+        if (a["class"]) == ['b_header']:
+            header = a.text.strip()
+        elif (a["class"]) == ['b_entry']:
+            try:
+                status = a.find("div", {"class": "b_status"}).text.strip()
+
+            except:
+                status = ""
+            description = header + " " + status
+            description = description.strip()
+            action_date = a.div.div.span.text.split(" ")
+            action_date = " ".join(action_date[:3])
+            try:
+                ad = datetime.datetime.strptime(action_date, '%b %d, %Y')
+                ad = ad.strftime('%Y-%m-%d')
+
+            except:
+                ad = None
+            action = {'date': ad, 'action_by': 'Legislative Assembly', 'description': description.replace('\n', " ")}
+            # print(action)
+            actions.append(action)
+    # print(actions)
+
+    actions.reverse()
+    row.actions = actions
+    current_status = actions[0]['description']
+    row.current_status = current_status
+    # print(row.actions)
+
+
+
+
+
+
+
+    # for div in bill_entries:
+
+
+
+
+
+
+
+
 
     return row
 
@@ -93,7 +215,7 @@ if __name__ == '__main__':
     print(bill_df)
 
     big_list_of_dicts = bill_df.to_dict('records')
-    # print(*big_list_of_dicts, sep="\n")
+    print(*big_list_of_dicts, sep="\n")
 
     print('Writing data to database...')
     scraper_utils.insert_legislation_data_into_db(big_list_of_dicts)
