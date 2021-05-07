9206d1401646e00450ea4ae6bc6d35986f456f91
Hyunjin Nha <hyunjinnha@gmail.com>
2021-05-07 07:46:23 -0700

    Completed refactoring of code.

--- scrapers/ca/ca-provinces/YT/legislators/ca_yt_legislator_scraper.py
+++ scrapers/ca/ca-provinces/YT/legislators/ca_yt_legislator_scraper.py
@@ -19,6 +19,7 @@ from nameparser import HumanName
 import pandas as pd
 import unidecode
 import numpy as np
+from tqdm import tqdm
 
 BASE_URL = 'https://yukonassembly.ca/'
 MLA_URL = 'https://yukonassembly.ca/mlas?field_party_affiliation_target_id=All&field_assembly_target_id=All&sort_by=field_last_name_value'
@@ -41,27 +42,79 @@ columns_not_on_main_site = ['birthday', 'education', 'occupation']
 scraper_utils = CAProvTerrLegislatorScraperUtils('YT', 'ca_yt_legislators')
 crawl_delay = scraper_utils.get_crawl_delay(BASE_URL)
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 def driver():
   main_page_soup = get_page_as_soup(MLA_URL)
   scraper_for_main = ScraperForMainSite()
 
+  print("Getting data from mla pages...")
   all_mla_links = scraper_for_main.get_all_mla_links(main_page_soup)
+  mla_data = get_data_from_all_links(get_mla_data, all_mla_links)
 
-  with Pool() as pool:
-    data = pool.map(func=get_mla_data,
-                    iterable=all_mla_links)
+  print("Getting data from wiki pages...")
+  individual_wiki_links = scrape_main_wiki_link(WIKI_URL)
+  wiki_data = get_data_from_all_links(scraper_utils.scrape_wiki_bio, individual_wiki_links)
 
-  # delete after... only here because need to test if main function at the end runs
-  mla_df = pd.DataFrame(data)
-  print(mla_df)
+  complete_data_set = configure_data(mla_data, wiki_data)
+
+  print('Writing data to database...')
+  scraper_utils.write_data(complete_data_set)
+  print("Complete")
 
 def get_page_as_soup(url):
   page_html = get_site_as_html(url)
   return soup(page_html, 'html.parser')
 
+def get_site_as_html(link_to_open):
+  uClient = urlopen(link_to_open)
+  page_html = uClient.read()
+  uClient.close()
+  return page_html
+
+def get_data_from_all_links(function, all_mla_links):
+  with Pool() as pool:
+    data = pool.map(func=function,
+                    iterable=all_mla_links)
+  return data
+
 def get_mla_data(mla_url):
   scraper_for_mla = ScraperForMLAs(mla_url)
   return scraper_for_mla.get_rows()
+
+def configure_data(mla_data, wiki_data):
+  mla_df = pd.DataFrame(mla_data)
+  mla_df = mla_df.drop(columns = columns_not_on_main_site)
+  
+  wiki_df = pd.DataFrame(wiki_data)[
+    ['birthday', 'education', 'name_first', 'name_last', 'occupation']
+  ]
+
+  big_df = pd.merge(mla_df, wiki_df, 
+                    how='left',
+                    on=['name_first', 'name_last'])
+  big_df['birthday'] = big_df['birthday'].replace({np.nan: None})
+  big_df['occupation'] = big_df['occupation'].replace({np.nan: None})
+  big_df['education'] = big_df['education'].replace({np.nan: None})
+
+  return big_df.to_dict('records')
+
+def scrape_main_wiki_link(wiki_link):
+    wiki_urls = []
+    page_html = get_site_as_html(wiki_link)
+    # # html parsing
+    page_soup = soup(page_html, "html.parser")
+
+    table = page_soup.find("table", {"class": "wikitable sortable"})
+    table = table.findAll("tr")[1:]
+    for tr in table:
+        td = tr.findAll("td")[1]
+        url = 'https://en.wikipedia.org' + (td.a["href"])
+
+        wiki_urls.append(url)
+    scraper_utils.crawl_delay(crawl_delay)
+    return wiki_urls
 
 class ScraperForMainSite:  
   def get_all_mla_links(self, main_page_soup):
@@ -90,28 +143,12 @@ class ScraperForMainSite:
       url = url.replace('\n', '')
       current_list_of_urls.append(url)
 
-def scrape_main_wiki_link(wiki_link):
-    wiki_urls = []
-    page_html = get_site_as_html(wiki_link)
-    # # html parsing
-    page_soup = soup(page_html, "html.parser")
-
-    table = page_soup.find("table", {"class": "wikitable sortable"})
-    table = table.findAll("tr")[1:]
-    for tr in table:
-        td = tr.findAll("td")[1]
-        url = 'https://en.wikipedia.org' + (td.a["href"])
-
-        wiki_urls.append(url)
-    scraper_utils.crawl_delay(crawl_delay)
-    return wiki_urls
-
 class ScraperForMLAs:
   def __init__(self, mla_url):
     self.row = scraper_utils.initialize_row()
     self.url = mla_url
     self.soup = get_page_as_soup(self.url)
-    self.main_container = soup.find('div', {'class' : 'content'})
+    self.main_container = self.soup.find('div', {'class' : 'content'})
     self.set_row_data()
 
   def get_rows(self):
@@ -127,6 +164,7 @@ class ScraperForMLAs:
     self.set_address()
     self.set_contact_info()
     self.set_service_period()
+    self.set_most_recent_term_id()
 
   def set_name_data(self):
     human_name = self.get_full_human_name()
@@ -156,7 +194,7 @@ class ScraperForMLAs:
     party_info = party_info_container.text
     party_name = self.edit_party_name(party_info)
     self.row.party = party_name
-    self.row.party_id = self.set_party_id(party_name)
+    self.set_party_id(party_name)
   
   def edit_party_name(self, party_name):
     if 'Liberal' in party_name:
@@ -179,9 +217,9 @@ class ScraperForMLAs:
   def set_address(self):
     full_address = self.get_address()
     address_location = full_address[0]
-    street_address = ' '.join(full_address[1], full_address[2])
+    street_address = full_address[1] + full_address[2]
     address_info = {'location' :  address_location, 'address' : street_address}
-    self.row.addresses = list(address_info)
+    self.row.addresses = address_info
   
   '''
   This function returns part of a list (parts_of_address).
@@ -194,7 +232,7 @@ class ScraperForMLAs:
     full_address = address_container.find('p').text
     full_address = full_address.replace('\xa0', '')
     parts_of_address = full_address.split('\n')
-    return parts_of_address[:2]
+    return parts_of_address[:3]
 
   def set_contact_info(self):
     contact_info = self.get_contact_info()
@@ -215,7 +253,8 @@ class ScraperForMLAs:
   so effectively our numbers parm from above method will store numbers as such
   '''
   def categorize_numbers(self, numbers):
-    categorized_numbers = [], i = 0
+    categorized_numbers = []
+    i = 0
     phone_types = ['phone', 'fax']
     for number in numbers:
       info = {'office' : phone_types[i],
@@ -226,6 +265,8 @@ class ScraperForMLAs:
 
   def set_service_period(self):
     in_service_paragraph = self.get_service_paragraph()
+    service_periods = self.get_service_periods(in_service_paragraph)
+    self.row.years_active = service_periods
   
   def get_service_paragraph(self):
     mla_summary_container = self.soup.find('div', {'class' : 'field--type-text-with-summary'})
@@ -237,161 +278,30 @@ class ScraperForMLAs:
       if 'elected to the Yukon Legislative Assembly' in paragraph.text:
         return paragraph.text
 
-    
+  def get_service_periods(self, paragraph):
+    service_periods_as_string = re.findall('\d\d[a-z]{2}', paragraph)
+    service_periods_as_int = [int(period[0:2]) for period in service_periods_as_string]
+    return self.get_service_periods_as_years(service_periods_as_int)
 
-# TODO: CLEAN UP CODE
-# Try and refactor each part to a separate function(like get name, add name data)
-
-def collect_mla_data(link_to_mla):
-  row = scraper_utils.initialize_row()
-  page_html = get_site_as_html(link_to_mla)
-  page_soup = soup(page_html, 'html.parser')
-
-  row.source_url = link_to_mla
-
-  main_content_container = page_soup.find('div', {'class' : 'content'})
-  full_name = main_content_container.find('span').text
-  
-  human_name = HumanName(full_name)
-
-  row.name_full = human_name.full_name
-  row.name_last = human_name.last
-  row.name_first = human_name.first
-  row.name_middle = human_name.middle
-  row.name_suffix = human_name.suffix
-
-  role_container = main_content_container.find('div', {'class' : 'field--name-field-title'})
-  party_info = main_content_container.find('div', {'class' : 'field--name-field-party-affiliation'}).text
-  
-  if 'Liberal' in party_info:
-    party_info = 'Liberal'
-  elif 'Democratic' in party_info:
-    party_info = 'New Democratic'
-  
-  row.party = party_info
-  try:
-    row.party_id = scraper_utils.get_party_id(party_info)
-  except:
-    row.party_id = 0
-
-  riding = main_content_container.find('div', {'class' : 'field--name-field-constituency'}).text
-  row.riding = riding
-
-  # Everyone has the same office address. That address is declared at the footer of page
-  page_footer = page_soup.find('footer')
-  address_container = page_footer.find('div', {'class' : 'footer-row--right'})
-  full_address = address_container.find('p').text
-  full_address = full_address.replace('\xa0', '')
-  parts_of_address = full_address.split('\n')
-
-  address_location = parts_of_address[0]
-
-  # This may not make sense but the address that is split has 6 entries, last two are not relevant
-  address = parts_of_address[1] + parts_of_address[2]
-  address_info = {'location' :  address_location, 'address' : address}
-  row.addresses = [address_info]
-
-  member_sidebar = page_soup.find('aside', {'class' : 'member-sidebar'})
-  contact_info_container = member_sidebar.find('article')
-  contact_info = contact_info_container.findAll('p')[1]
-
-  email_address = contact_info.a.text
-  row.email = email_address
-
-  phone_number = re.findall(r'[0-9]{3}-[0-9]{3}-[0-9]{4}', contact_info.text)
-  phone_info = []
-  i = 0
-  phone_types = ['Phone', 'Fax']
-  for number in phone_number:
-    info = {'office' : phone_types[i],
-                  'number' : number}
-    phone_info.append(info)
-    i = i + 1
-  row.phone_numbers = phone_info
-
-  mla_summary_container = page_soup.find('div', {'class' : 'field--type-text-with-summary'})
-  mla_summary_paragraphs = mla_summary_container.findAll('p')
-  
-  in_service_paragraph = ''
-  for paragraph in mla_summary_paragraphs:
-    if 'elected to the Yukon Legislative Assembly' in paragraph.text:
-      in_service_paragraph = paragraph.text
-     
-  service_periods = re.findall('\d\d[a-z]{2}', in_service_paragraph)
-  service_periods_as_int = []
-  for period in service_periods:
-    service_periods_as_int.append(int(period[0:2]))
-
-  service_periods_as_years = []
-  last_period = list(NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR)[-1]
-  for period in service_periods_as_int:
+  def get_service_periods_as_years(self, service_periods_as_int):
+    service_periods_as_years = []
+    for period in service_periods_as_int:
+      self.add_periods_as_years(period, service_periods_as_years)
+    return service_periods_as_years
+      
+  def add_periods_as_years(self, period, return_list):
+    last_period = list(NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR)[-1]
     current_term_year = NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR.get(period)
     if period != last_period:
-      next_period = period + 1
-      next_term_year = NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR.get(next_period)
+      next_term_year = NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR.get(period + 1)
       for i in range(current_term_year, next_term_year):
-        service_periods_as_years.append(i)
+        return_list.append(i)
     elif CURRENT_YEAR > period:
       for i in range(current_term_year, CURRENT_YEAR + 1):
-        service_periods_as_years.append(i)
+        return_list.append(i)
 
-  row.years_active = service_periods_as_years
-
-  longest_service_period = find_longest_thread(service_periods_as_int)
-  most_recent_term = longest_service_period[-1]
-
-  row.most_recent_term_id = NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR.get(most_recent_term)
-
-  scraper_utils.crawl_delay(crawl_delay)
-  return row
-
-# !!!Might want to add this to general functions
-def find_longest_thread(array_of_ints):
-  if len(array_of_ints) == 1:
-    return array_of_ints
-  
-  return_array = [array_of_ints[0]]
-  for i in range(1, len(array_of_ints)):
-    if array_of_ints[i] == return_array[-1] + 1:
-      return_array.append(array_of_ints[i])
-    else:
-      return_array.clear()
-      return_array.append(array_of_ints[i])
-  return return_array
-
-# Repeated so I wanted to extract as function
-def get_site_as_html(link_to_open):
-  uClient = urlopen(link_to_open)
-  page_html = uClient.read()
-  uClient.close()
-  return page_html
+  def set_most_recent_term_id(self):
+    self.row.most_recent_term_id = self.row.years_active[-1]
 
 if __name__ == '__main__':
-  pd.set_option('display.max_rows', None)
-  pd.set_option('display.max_columns', None)
   driver()
-
-  # with Pool() as pool:
-  #   data = pool.map(func=collect_mla_data, iterable=individual_mla_links)
-  
-  # mla_df = pd.DataFrame(data)
-  # mla_df = mla_df.drop(columns = columns_not_on_main_site)
-
-  # individual_wiki_links = scrape_main_wiki_link(WIKI_URL)
-  # with Pool() as pool:
-  #   wiki_data = pool.map(func=scraper_utils.scrape_wiki_bio, iterable=individual_wiki_links)
-  # wiki_df = pd.DataFrame(wiki_data)[
-  #     ['birthday', 'education', 'name_first', 'name_last', 'occupation']]
-
-  # big_df = pd.merge(mla_df, wiki_df, how='left',
-  #                   on=['name_first', 'name_last'])
-  # big_df['birthday'] = big_df['birthday'].replace({np.nan: None})
-  # big_df['occupation'] = big_df['occupation'].replace({np.nan: None})
-  # big_df['education'] = big_df['education'].replace({np.nan: None})
-  
-  # big_list_of_dicts = big_df.to_dict('records')
-  # print('Writing data to database...')
-
-  # scraper_utils.write_data(big_list_of_dicts)
-
-  print('Complete!')
