c7105a27b7c68a60564d82967456999bd96a4fab
Hyunjin Nha <hyunjinnha@gmail.com>
2021-05-07 07:57:01 -0700

    Made class functions private.

--- scrapers/ca/ca-provinces/YT/legislators/ca_yt_legislator_scraper.py
+++ scrapers/ca/ca-provinces/YT/legislators/ca_yt_legislator_scraper.py
@@ -149,54 +149,55 @@ class ScraperForMLAs:
     self.url = mla_url
     self.soup = get_page_as_soup(self.url)
     self.main_container = self.soup.find('div', {'class' : 'content'})
-    self.set_row_data()
+    self.__set_row_data()
 
   def get_rows(self):
     return self.row
 
-  def set_row_data(self):
+  def __set_row_data(self):
     self.row.source_url = self.url
-    self.set_name_data()
-    self.set_role_data()
-    self.set_party_data()
-    self.set_riding_data()
+    self.__set_name_data()
+    self.__set_role_data()
+    self.__set_party_data()
+    self.__set_riding_data()
     # Everyone has the same office address. That address is declared at the footer of page
-    self.set_address()
-    self.set_contact_info()
-    self.set_service_period()
-    self.set_most_recent_term_id()
+    self.__set_address()
+    self.__set_contact_info()
+    self.__set_service_period()
+    self.__set_most_recent_term_id()
+    self.__set_committee_data()
 
-  def set_name_data(self):
-    human_name = self.get_full_human_name()
+  def __set_name_data(self):
+    human_name = self.__get_full_human_name()
     self.row.name_full = human_name.full_name
     self.row.name_last = human_name.last
     self.row.name_first = human_name.first
     self.row.name_middle = human_name.middle
     self.row.name_suffix = human_name.suffix
 
-  def get_full_human_name(self):
+  def __get_full_human_name(self):
     full_name = self.main_container.find('span').text
     full_name = full_name.replace('hon', '').strip()
     return HumanName(full_name)
   
-  def set_role_data(self):
+  def __set_role_data(self):
     role_container = self.main_container.find('div', {'class' : 'field--name-field-title'})
-    self.row.role = self.assign_role(role_container)
+    self.row.role = self.__assign_role(role_container)
 
-  def assign_role(self, role):
+  def __assign_role(self, role):
     if role == None:
       return "Member of the Legislative Assembly"
     else:
       return role.text
 
-  def set_party_data(self):
+  def __set_party_data(self):
     party_info_container = self.main_container.find('div', {'class' : 'field--name-field-party-affiliation'})
     party_info = party_info_container.text
-    party_name = self.edit_party_name(party_info)
+    party_name = self.__edit_party_name(party_info)
     self.row.party = party_name
-    self.set_party_id(party_name)
+    self.__set_party_id(party_name)
   
-  def edit_party_name(self, party_name):
+  def __edit_party_name(self, party_name):
     if 'Liberal' in party_name:
       return 'Liberal'
     elif 'Democratic' in party_name:
@@ -204,18 +205,18 @@ class ScraperForMLAs:
     else:
       return party_name
   
-  def set_party_id(self, party_name):
+  def __set_party_id(self, party_name):
     try:
       self.row.party_id = scraper_utils.get_party_id(party_name)
     except:
       row.party_id = 0
 
-  def set_riding_data(self):
+  def __set_riding_data(self):
     riding = self.main_container.find('div', {'class' : 'field--name-field-constituency'}).text
     self.row.riding = riding
 
-  def set_address(self):
-    full_address = self.get_address()
+  def __set_address(self):
+    full_address = self.__get_address()
     address_location = full_address[0]
     street_address = full_address[1] + full_address[2]
     address_info = {'location' :  address_location, 'address' : street_address}
@@ -226,7 +227,7 @@ class ScraperForMLAs:
   This is because only the first three from the split address is relevant.
   If html structure changes, this may need to be fixed.
   '''
-  def get_address(self):
+  def __get_address(self):
     page_footer = self.soup.find('footer')
     address_container = page_footer.find('div', {'class' : 'footer-row--right'})
     full_address = address_container.find('p').text
@@ -234,25 +235,25 @@ class ScraperForMLAs:
     parts_of_address = full_address.split('\n')
     return parts_of_address[:3]
 
-  def set_contact_info(self):
-    contact_info = self.get_contact_info()
+  def __set_contact_info(self):
+    contact_info = self.__get_contact_info()
     self.row.email = contact_info.a.text
-    self.row.phone_numbers = self.get_phone_numbers(contact_info)
+    self.row.phone_numbers = self.__get_phone_numbers(contact_info)
 
-  def get_contact_info(self):
+  def __get_contact_info(self):
     profile_sidebar = self.soup.find('aside', {'class' : 'member-sidebar'})
     contact_info_container = profile_sidebar.find('article')
     return contact_info_container.findAll('p')[1]
   
-  def get_phone_numbers(self, contact_info):
+  def __get_phone_numbers(self, contact_info):
     numbers = re.findall(r'[0-9]{3}-[0-9]{3}-[0-9]{4}', contact_info.text)
-    return self.categorize_numbers(numbers)
+    return self.__categorize_numbers(numbers)
 
   '''
   phone_types is in this order because the website has the numbers ordered from phone then fax,
   so effectively our numbers parm from above method will store numbers as such
   '''
-  def categorize_numbers(self, numbers):
+  def __categorize_numbers(self, numbers):
     categorized_numbers = []
     i = 0
     phone_types = ['phone', 'fax']
@@ -263,33 +264,33 @@ class ScraperForMLAs:
     i = i + 1
     return categorized_numbers
 
-  def set_service_period(self):
-    in_service_paragraph = self.get_service_paragraph()
-    service_periods = self.get_service_periods(in_service_paragraph)
+  def __set_service_period(self):
+    in_service_paragraph = self.__get_service_paragraph()
+    service_periods = self.__get_service_periods(in_service_paragraph)
     self.row.years_active = service_periods
   
-  def get_service_paragraph(self):
+  def __get_service_paragraph(self):
     mla_summary_container = self.soup.find('div', {'class' : 'field--type-text-with-summary'})
     mla_summary_paragraphs = mla_summary_container.findAll('p')
-    return self.find_key_sentence_from_paragraph(mla_summary_paragraphs)
+    return self.__find_key_sentence_from_paragraph(mla_summary_paragraphs)
 
-  def find_key_sentence_from_paragraph(self, paragraphs):
+  def __find_key_sentence_from_paragraph(self, paragraphs):
     for paragraph in paragraphs:
       if 'elected to the Yukon Legislative Assembly' in paragraph.text:
         return paragraph.text
 
-  def get_service_periods(self, paragraph):
+  def __get_service_periods(self, paragraph):
     service_periods_as_string = re.findall('\d\d[a-z]{2}', paragraph)
     service_periods_as_int = [int(period[0:2]) for period in service_periods_as_string]
-    return self.get_service_periods_as_years(service_periods_as_int)
+    return self.__get_service_periods_as_years(service_periods_as_int)
 
-  def get_service_periods_as_years(self, service_periods_as_int):
+  def __get_service_periods_as_years(self, service_periods_as_int):
     service_periods_as_years = []
     for period in service_periods_as_int:
-      self.add_periods_as_years(period, service_periods_as_years)
+      self.__add_periods_as_years(period, service_periods_as_years)
     return service_periods_as_years
       
-  def add_periods_as_years(self, period, return_list):
+  def __add_periods_as_years(self, period, return_list):
     last_period = list(NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR)[-1]
     current_term_year = NTH_LEGISLATIVE_ASSEMBLY_TO_YEAR.get(period)
     if period != last_period:
@@ -300,8 +301,11 @@ class ScraperForMLAs:
       for i in range(current_term_year, CURRENT_YEAR + 1):
         return_list.append(i)
 
-  def set_most_recent_term_id(self):
+  def __set_most_recent_term_id(self):
     self.row.most_recent_term_id = self.row.years_active[-1]
+
+  def __set_committee_data(self):
+    pass
 
 if __name__ == '__main__':
   driver()
