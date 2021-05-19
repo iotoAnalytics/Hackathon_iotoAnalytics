94a20efaa245ae2754b1f7b629a188f0f028d13e
Kat Sukkau <Katsukkau@gmail.com>
2021-05-19 12:00:36 -0700

    completed NS legislation and legislator scrapers

--- scrapers/ca/ca-provinces/NS/legislation/ca_ns_legislation_scraper.py
+++ scrapers/ca/ca-provinces/NS/legislation/ca_ns_legislation_scraper.py
@@ -29,7 +29,7 @@ from multiprocessing import Pool
 import requests
 from bs4 import BeautifulSoup
 from scraper_utils import CAProvinceTerrLegislationScraperUtils
-
+import dateutil.parser as dparser
 
 prov_terr_abbreviation = 'NS'
 database_table_name = 'ca_ns_legislation'
@@ -76,8 +76,9 @@ def get_session(main_div, row):
     table = main_div.find('table', {'class': 'views-table'})
     session = table.findAll('td')[2].text
     row.session = session
-    return_session = session.split(', ')[1]
-    return_session = return_session.replace(' ', '')
+    session = session.replace(',', '')
+    session = session.split(' ')
+    return_session = session[1] + "({s})".format(s=session[3])
     return return_session
 
 
@@ -111,14 +112,85 @@ def get_actions(main_div, row):
     actions = []
     table = main_div.find('table', {'class': 'bill-metadata-table'})
     table_row = table.findAll('tr')
-    for tr in reversed(table_row):
-        status = tr.findAll('td')[0].text
-        date = tr.findAll('td')[1].text
-        if status:
-            if date:
-                action = {'date': date, 'action_by': 'Legislative Assembly', 'description': status}
-                actions.append(action)
-    print(actions)
+    try:
+        for tr in reversed(table_row):
+            status = tr.findAll('td')[0].text
+            date = tr.findAll('td')[1].text
+            try:
+                date = dparser.parse(date, fuzzy=True)
+                date = date.strftime("%Y-%m-%d")
+            except Exception:
+                date = None
+            if status:
+                if date:
+                    action = {'date': date, 'action_by': 'Legislative Assembly', 'description': status}
+                    actions.append(action)
+    except Exception:
+        pass
+
+    row.actions = actions
+
+
+def get_get_date_introduced(main_div, row):
+    table = main_div.find('table', {'class': 'bill-metadata-table'})
+    table_row = table.findAll('tr')
+    introduced_text = table_row[1].findAll('td')[1].text
+    date = dparser.parse(introduced_text, fuzzy=True)
+    date = date.strftime("%Y-%m-%d")
+    row.date_introduced = date
+
+
+def get_bill_description(main_div, row):
+    text = main_div.find('div', {'class': 'pane-ns-leg-bill-metadata'}).text
+    text = text.split('Introduced')[0]
+    text = text[text.index('An'):]
+    text = text.replace('\n', ' ')
+    row.bill_description = text
+
+
+def get_bill_text(url, row):
+    page = scraper_utils.request(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    bill_text = soup.find('div', {'class': 'bill_text'}).text
+
+    scraper_utils.crawl_delay(crawl_delay)
+    row.bill_text = bill_text
+
+
+def get_bill_link(main_div, row):
+    table = main_div.find('table', {'class': 'bill-metadata-table'})
+    table_row = table.findAll('tr')
+    try:
+        for tr in table_row:
+            text = tr.text
+            if "Statute" in text:
+                link = tr.find('a').get('href')
+            elif "Bill as introduced" in text:
+                link = tr.find('a').get('href')
+        url = base_url + link
+        get_bill_text(url, row)
+    except Exception:
+        pass
+
+
+def get_sponsor_id(name_first, name_last):
+    search_for = dict(name_last=name_last, name_first=name_first)
+    sponsor_id = scraper_utils.get_legislator_id(**search_for)
+    return sponsor_id
+
+
+def get_principal_sponsor(main_div, row):
+    text = main_div.find('div', {'class': 'pane-ns-leg-bill-metadata'}).text
+    text = text.split('Introduced by ')[1]
+    name = text.split(',')[0]
+    hn = HumanName(name)
+    name_full = name
+    name_last = hn.last
+    name_first = hn.first
+
+    row.principal_sponsor = name_full
+    row.principal_sponsor_id = get_sponsor_id(name_first, name_last)
+
 
 def scrape(url):
     '''
@@ -144,6 +216,7 @@ def scrape(url):
     source_url = base_url + url
     row.source_url = source_url
     row.region = scraper_utils.get_region(prov_terr_abbreviation)
+    row.chamber_origin = 'Legislative Assembly'
 
     page = scraper_utils.request(source_url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -156,126 +229,14 @@ def scrape(url):
     goverlytics_id = f'{prov_terr_abbreviation}_{session}_{bill_name}'
     row.goverlytics_id = goverlytics_id
 
-    # get_bill_type(main_div, row)
-    #
-    # row.chamber_origin = 'Legislative Assembly'
-    #
-    # get_bill_title(main_div, row)
-    # get_current_status(main_div, row)
+    get_bill_type(main_div, row)
+    get_bill_title(main_div, row)
+    get_current_status(main_div, row)
     get_actions(main_div, row)
-
-    # chamber_origin = ''
-    # bill_type = ''
-    # if 'HB' == doc_type:
-    #     chamber_origin = 'House'
-    #     bill_type = 'Bill'
-    # elif ...:
-        # Check for other types like SB (senate bills), HRes (House Resolutions), etc.
-        # For now we're only work with HB bills so we'll keep it simple
-
-
-
-
-
-    # The Illinois state legislation website has their data stored in a weird way...
-    # everything is stored in spans so we're gonna try pulling the data we need from
-    # those. Your implementations will probably look quite a bit different than this.
-
-    # Get bill description and summary
-    # bill_description = ''
-    # bill_summary = ''
-    # spans = soup.findAll('span')
-    # for idx, span in enumerate(spans):
-    #     txt = span.text
-    #     if 'Short Description:' in txt:
-    #         bill_description = spans[idx + 1].text
-    #     if 'Synopsis As Introduced' in txt:
-    #         bill_summary = spans[idx + 1].text.strip()
-    # row.bill_description = bill_description
-    # row.bill_summary = bill_summary
-    #
-    # # Get bill sponsors
-    # table = soup.find(
-    #     'table', {'width': '440', 'border': '0', 'align': 'left'})
-    # table_td = table.find('td', {'width': '100%'})
-    #
-    # a_tag = table_td.findAll('a', href=True)
-    # sponsors = []
-    # for a in a_tag:
-    #     if '/house/Rep.asp' in a['href'] or '/senate/Senator.asp' in a['href']:
-    #         sponsors.append(a.text)
-
-    # # We'll now try to get the legislator goverlytics ID. Fortunately for us, this
-    # # site provides a unique identifier for each legislator. Normally we would do
-    # # the following:
-    # sponsor_id = scraper_utils.get_legislator_id(state_member_id=legislator_id)
-    # # However, since this is often not the case, we will search for the id using the
-    # # legislator name. We are given the legislator's full name, but if you are given
-    # # only the legislator initials and last name, which is more often the case, be sure to
-    # # use the legislators_search_startswith() method, which might look something like this:
-    # sponsor_id = scraper_utils.legislators_search_startswith('goverlytics_id', 'name_first', first_initial, name_last=name_last)
-
-    # sponsors_id = []
-    # for sponsor in sponsors:
-    #     hn = HumanName(sponsor)
-    #     name_first = hn.first
-    #     name_middle = hn.middle
-    #     name_last = hn.last
-    #     name_suffix = hn.suffix
-    #
-    #     search_for = dict(name_first=name_first, name_middle=name_middle,
-    #                       name_last=name_last, name_suffix=name_suffix)
-    #
-    #     sponsor_id = scraper_utils.get_legislator_id(**search_for)
-    #
-    #     # Some sponsor IDs weren't found, so we won't include these.
-    #     # If you are unable to find legislators based on the provided search criteria, be
-    #     # sure to investigate. Check the database and make sure things like names match
-    #     # exactly, including case and diacritics.
-    #     if sponsor_id is not None:
-    #         sponsors_id.append(sponsor_id)
-    #
-    # row.sponsors = sponsors
-    # row.sponsors_id = sponsors_id
-    #
-    # # Get actions
-    # actions_table = soup.findAll('table', {
-    #                              'width': '600', 'cellspacing': '0', 'cellpadding': '2', 'bordercolor': 'black', 'border': '1'})[1]
-    #
-    # action_date = ''
-    # action_by = ''
-    # action_description = ''
-    # actions = []
-    # number_of_columns = 3
-    # # Skip the header row
-    # for idx, td in enumerate(actions_table.findAll('td')[3:]):
-    #     # With this type of method, normally you would search by 'tr' and then grab the value
-    #     # from each 'td' in the row, but for some reason, beautiful soup wasn't able to find
-    #     # the 'tr' so I had to get the value using a different, less intuitive method.
-    #     mod = idx % number_of_columns
-    #     if mod == 0:
-    #         action_date = td.text.strip()
-    #     if mod == 1:
-    #         action_by = td.text.strip()
-    #     if mod == 2:
-    #         action_description = td.text.strip()
-    #         actions.append(
-    #             dict(date=action_date, action_by=action_by, description=action_description))
-    #
-    # # We can get the date introduced from the first action, and the current status from
-    # # the most recent action.
-    # date_introduced = None
-    # current_status = ''
-    # if len(actions) > 0:
-    #     date_introduced = datetime.datetime.strptime(
-    #         actions[0]['date'], '%m/%d/%Y')
-    #     current_status = actions[-1]['description']
-    #
-    # row.actions = actions
-    # row.current_status = current_status
-    # row.date_introduced = date_introduced
-
-    # There's more data on other pages we can collect, but we have enough data for this demo!
+    get_bill_description(main_div, row)
+    get_bill_link(main_div, row)
+    get_principal_sponsor(main_div, row)
+    get_get_date_introduced(main_div, row)
 
     # Delay so we do not overburden servers
     scraper_utils.crawl_delay(crawl_delay)
@@ -299,6 +260,6 @@ like names match exactly, including case and diacritics.\n~~~~~~~~~~~~~~~~~~~')
         data = pool.map(scrape, urls)
 
     # Once we collect the data, we'll write it to the database.
-    #scraper_utils.write_data(data)
+    scraper_utils.write_data(data)
 
     print('Complete!')
