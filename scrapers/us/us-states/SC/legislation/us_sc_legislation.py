af19bed27573d5efaaccf220e871db5c63da2295
Sleung124 <samuelleung124@gmail.com>
2021-06-01 16:11:11 -0700

    added vote scrapping, need to fix committee adding

--- scrapers/us/us-states/SC/legislation/us_sc_legislation.py
+++ scrapers/us/us-states/SC/legislation/us_sc_legislation.py
@@ -12,6 +12,7 @@ import requests
 import time
 from multiprocessing import Pool
 from itertools import chain
+import pandas as pd
 from database import Database
 import configparser
 from pprint import pprint
@@ -20,7 +21,7 @@ import re
 import boto3
 
 state_abbreviation = 'SC'
-database_table_name = 'us_sc_legislation_test'
+database_table_name = 'us_sc_legislation'
 legislator_table_name = 'us_sc_legislators'
 
 scraper_utils = USStateLegislationScraperUtils(
@@ -31,6 +32,12 @@ house_bills = 'https://www.scstatehouse.gov/sessphp/hintros.php'
 base_url = 'https://www.scstatehouse.gov'
 # Get the crawl delay specified in the website's robots.txt file
 crawl_delay = scraper_utils.get_crawl_delay(base_url)
+
+
+# Splits list 'a' into 'n' parts
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
 def get_daily_bills(bill_list_link_list):
@@ -57,6 +64,36 @@ def get_bill_links(daily_bills_page):
     return return_links
 
 
+def get_vote_info(vote_link):
+    request = requests.get(vote_link)
+    soup = BeautifulSoup(request.content, 'lxml')
+    info_dict = pd.read_html(str(soup.find('table')))[0].to_dict('records')[1:]
+    vote_list = []
+    for item in info_dict:
+        date = item['Date/Time'].split('\xa0')[0]
+        description = item['Motion'].replace('\xa0', ' ')
+        yea = item['Yeas']
+        nay = item['Nays']
+        nv = item['N/V']
+        absent = item['Exc.Abs.']
+        total = item['Total']
+        if item['Result'] == 'Passed':
+            passed = 1
+        else:
+            passed = 0
+        vote_list.append({
+            'date': date,
+            'description': description,
+            'passed': passed,
+            'yea': yea,
+            'nay': nay,
+            'nv': nv,
+            'absent': absent,
+            'total': total
+        })
+    return vote_list
+
+
 def get_actions(bill_link):
     request = requests.get(bill_link)
     soup = BeautifulSoup(request.content, 'lxml')
@@ -78,6 +115,7 @@ def get_actions(bill_link):
 
 
 def edit_sponsors(sponsor_text):
+    legislator_type = ''
     if 'Senator' in sponsor_text or 'Senators' in sponsor_text:
         if 'Senators' in sponsor_text:
             legislator_type = 'Senators'
@@ -144,10 +182,18 @@ def scrape(url):
             action_list.append(action_item)
     current_action = action_list[-1]
     row.actions = action_list
-    row.current_status = current_action
+    row.current_status = current_action['description']
 
-    full_text_link = soup.find_all('a', {'class': 'nodisplay'})[1].get('href')
+    display_links = soup.find_all('a', {'class': 'nodisplay'})
+    full_text_link = display_links[1].get('href')
     full_text_link = base_url + full_text_link
+
+    try:
+        vote_link = base_url + display_links[2].get('href')
+        row.votes = get_vote_info(vote_link)
+    except:
+        pass
+
     request1 = requests.get(full_text_link)
     soup1 = BeautifulSoup(request1.content, 'lxml')
     text = soup1.text.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').strip()
@@ -158,6 +204,7 @@ def scrape(url):
     row.bill_summary = [x for x in summary_split if x][1].split('View full text')[0]
 
     p_list = soup1.find_all('p')
+    sponsors = []
     for item in p_list:
         if item.text.strip().lower() == 'status information':
             num = p_list.index(item) + 1
@@ -165,31 +212,37 @@ def scrape(url):
 
             for el in sponsors_list:
                 if 'Sponsors:' in el:
-                    sponsors = el.replace('Sponsors:', ' ').strip().replace('\r', ' ')
-                    sponsors = edit_sponsors(sponsors)
+                    sponsors_raw = el.replace('Sponsors:', ' ').strip().replace('\r', ' ')
+                    sponsors = edit_sponsors(sponsors_raw)
 
-    if len(sponsors) == 1:
+    if sponsors == []:
+        pass
+    elif len(sponsors) == 1:
         principal_sponsor = sponsors[0].title()
         row.principal_sponsor = principal_sponsor
         if ' ' in principal_sponsor:
-            p_sponsor_split = principal_sponsor.split()
-            legislators_dict = scraper_utils.search_for_legislators(name_last=p_sponsor_split[1]).to_dict('records')
-            if len(legislators_dict) == 1:
-                row.principal_sponsor_id = legislators_dict[0]['goverlytics_id']
-            else:
-                params = [x for x in p_sponsor_split[0].split('.') if x]
-                if len(params) == 1:
-                    middle_name = False
+            try:
+                p_sponsor_split = principal_sponsor.split()
+                if p_sponsor_split[1] == 'I' or p_sponsor_split[1] == 'A' or p_sponsor_split[1] == 'E':
+                    print(f'\n{p_sponsor_split}\n')
+                legislators_dict = scraper_utils.search_for_legislators(name_last=p_sponsor_split[1]).to_dict('records')
+                if len(legislators_dict) == 1:
+                    row.principal_sponsor_id = legislators_dict[0]['goverlytics_id']
                 else:
-                    middle_name = True
-                for item in legislators_dict:
-                    if middle_name:
-                        try:
-                            if item['name_middle'][0].upper() == params[1] and item['name_first'][0].upper() == params[0]:
+                    params = [x for x in p_sponsor_split[0].split('.') if x]
+                    if len(params) == 1:
+                        middle_name = False
+                    else:
+                        middle_name = True
+                    for item in legislators_dict:
+                        if middle_name:
+                            if item['name_middle'][0].upper() == params[1] and item['name_first'][0].upper() == params[
+                                0]:
                                 if item['role'] == role:
                                     row.principal_sponsor_id = item['goverlytics_id']
-                        except IndexError:
-                            pass
+            except:
+                pass
+
         else:
             search_for = dict(name_last=principal_sponsor.title(), role=role)
             row.principal_sponsor_id = scraper_utils.get_legislator_id(**search_for)
@@ -199,28 +252,33 @@ def scrape(url):
         sponsor_ids = []
         for sponsor in sponsors:
             if ' ' in sponsor:
-                sponsor_split = sponsor.split()
-                legislators_dict = scraper_utils.search_for_legislators(name_last=sponsor_split[1]).to_dict('records')
-                if len(legislators_dict) == 1:
-                    sponsor_ids.append(legislators_dict[0]['goverlytics_id'])
-                else:
-                    params = [x for x in sponsor_split[0].split('.') if x]
-                    if len(params) == 1:
-                        middle_name = False
+                try:
+                    sponsor_split = sponsor.split()
+                    if sponsor_split[1] == 'I' or sponsor_split[1] == 'A' or sponsor_split[1] == 'E':
+                        print(f'\n{sponsor_split}\n{sponsor}\n{url}\n')
+                    legislators_dict = scraper_utils.search_for_legislators(name_last=sponsor_split[1]).to_dict(
+                        'records')
+                    if len(legislators_dict) == 1:
+                        sponsor_ids.append(legislators_dict[0]['goverlytics_id'])
                     else:
-                        middle_name = True
-                    for item in legislators_dict:
-                        if middle_name:
-                            try:
-                                if item['name_middle'][0].upper() == params[1] and\
+                        params = [x for x in sponsor_split[0].split('.') if x]
+                        if len(params) == 1:
+                            middle_name = False
+                        else:
+                            middle_name = True
+                        for item in legislators_dict:
+                            if middle_name:
+                                if item['name_middle'][0].upper() == params[1] and \
                                         item['name_first'][0].upper() == params[0]:
                                     if item['role']:
                                         sponsor_ids.append(item['goverlytics_id'])
-                            except IndexError:
-                                pass
+                except:
+                    pass
+
             else:
                 search_for = dict(name_last=sponsor, role=role)
                 sponsor_ids.append(scraper_utils.get_legislator_id(**search_for))
+        row.sponsors_id = sponsor_ids
 
     row.goverlytics_id = f'{state_abbreviation}_{session}_{bill_name}'
     scraper_utils.crawl_delay(crawl_delay)
@@ -241,10 +299,15 @@ if __name__ == '__main__':
     print(bill_links)
     print(f'TIME: {time.time() - start_time} SECONDS')
 
-    with Pool() as pool:
-        data = pool.map(scrape, bill_links[:5])
+    data = []
+    split_bill_links = list(split(bill_links, 10))
+    for bill_list in split_bill_links:
+        with Pool() as pool:
+            data += pool.map(scrape, bill_list)
+        print('Resting...')
+        time.sleep(3)
 
     # Once we collect the data, we'll write it to the database.
-    # scraper_utils.write_data(data)
+    scraper_utils.write_data(data)
 
     print('Complete!')
