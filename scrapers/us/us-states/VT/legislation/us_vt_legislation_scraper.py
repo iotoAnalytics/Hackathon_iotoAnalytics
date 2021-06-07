32aecf7058f7d26b26e1b5620623bb1cbeca5764
nickluong97@outlook.com <nickluong97@outlook.com>
2021-06-07 15:50:59 -0700

    Fixing some bugs and writing to database.

--- scrapers/us/us-states/VT/legislation/us_vt_legislation_scraper.py
+++ scrapers/us/us-states/VT/legislation/us_vt_legislation_scraper.py
@@ -7,7 +7,10 @@ from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import pandas as pd
 from multiprocessing import Pool
 from time import sleep
@@ -75,7 +78,7 @@ def get_urls(path):
         select = Select(driver.find_element_by_name('resolutions-introduced_length'))
 
     select.select_by_visible_text('All')
-    sleep(1)
+    sleep(1.5)
 
     try:
         table = driver.find_element_by_id('resolutions-introduced').find_element_by_tag_name('tbody')
@@ -84,7 +87,7 @@ def get_urls(path):
 
     all_rows = table.find_elements_by_tag_name('tr')
 
-    pbar = tqdm(all_rows[:40])
+    pbar = tqdm(all_rows)
     for row in pbar:
         pbar.set_description('Grabbing URLs')
         link = row.find_element_by_tag_name('a').get_attribute('href')
@@ -100,13 +103,15 @@ def get_goverlytics_id(soup, row):
     :param soup: soup object
     :param row: legislation row
     """
+    try:
+        bill_name = soup.find('div', {'class': 'bill-title'}).text.split()[0].replace('\t', '').replace('\n', '').replace('.', '')
+        year = soup.find('dl', {'class': 'summary-table'}).find_all('dd')[-1].find('a').text.split()[1].split('/')[-1]
+        row.goverlytics_id = f'{state_abbreviation}_{year}_{bill_name}'
+    except AttributeError:
+        pass
 
-    bill_name = soup.find('div', {'class': 'bill-title'}).text.split()[0].replace('\t', '').replace('\n', '').replace('.', '')
-    year = soup.find('dl', {'class': 'summary-table'}).find_all('dd')[-1].find('a').text.split()[1].split('/')[-1]
-    row.goverlytics_id = f'{state_abbreviation}_{year}_{bill_name}'
 
-
-def get_session(soup, row):
+def get_bill_session(soup, row):
     """
     Gets legislation session.
     :param soup: soup object
@@ -128,16 +133,15 @@ def get_bill_summary(soup, row):
     row.bill_summary = bill_summary
 
 
-def get_bill_sponsor_and_committees(soup, row):
+def get_bill_sponsor(soup, row):
     """
-    Gets sponsors and committees as they are sometimes in the same field.
+    Gets sponsors info.
 
     :param soup: soup object
     :param row: legislation row
     """
 
     sponsors_names = []
-    committees = []
 
     sponsors = soup.find('dl', {'class': 'summary-table'}).find('dd').find_all('li')
     for sponsor in sponsors:
@@ -146,8 +150,7 @@ def get_bill_sponsor_and_committees(soup, row):
             if sponsor == 'Less…':
                 pass
             elif 'committee' in sponsor.lower():
-                chamber = sponsor.split()[0]
-                committees.append({'chamber': chamber, 'committee': sponsor})
+                pass
             else:
                 name = sponsor.split()
                 sponsors_names.append(f'{name[-1]}, {name[1]}')
@@ -155,7 +158,6 @@ def get_bill_sponsor_and_committees(soup, row):
             pass
 
     row.sponsors = sponsors_names
-    row.committees = committees
 
 
 def get_current_status(soup, row):
@@ -179,12 +181,15 @@ def get_bill_text(soup, row):
     """
 
     bill_path = soup.find('ul', {'class': 'bill-path'})
-    bill_texts = bill_path.find_all('li')
-    latest_bill_text_endpoint = bill_texts[-1].find('a').get('href').replace(' ', '%20')
-    link = BASE_URL + latest_bill_text_endpoint
-    parsed = parser.from_file(link)
+    try:
+        bill_texts = bill_path.find_all('li')
+        latest_bill_text_endpoint = bill_texts[-1].find('a').get('href').replace(' ', '%20')
+        link = BASE_URL + latest_bill_text_endpoint
+        parsed = parser.from_file(link)
+        row.bill_text = parsed['content']
+    except AttributeError:
+        pass
 
-    row.bill_text = parsed['content']
 
 @scraper_utils.Timer()
 def get_bill_actions(url, row):
@@ -199,42 +204,96 @@ def get_bill_actions(url, row):
     select = Select(driver.find_element_by_name('bill-detailed-status-table_length'))
     select.select_by_visible_text('All')
     table = driver.find_element_by_id('bill-detailed-status-table').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
-    for tr in table:
-        r = tr.find_elements_by_tag_name('td')
-        sleep(3.5)
-        actions.append({'action_by': str(r[0].text), 'date': str(r[1].text), 'description': str(r[-1].text)})
-    row.actions = actions
-    driver.quit()
-
+    try:
+        for tr in table:
+            r = tr.find_elements_by_tag_name('td')
+            sleep(3)
+            actions.append({'action_by': str(r[0].text), 'date': str(r[1].text), 'description': str(r[-1].text)})
+        row.actions = actions
+        driver.quit()
+    except StaleElementReferenceException:
+        print('STALE ' + url)
+    except IndexError:
+        print('Index ' + url)
 
 @scraper_utils.Timer()
 def get_bill_votes(url, row):
     votes = []
     driver = open_driver(url)
+    tab = driver.find_element_by_class_name('tab-content').find_element_by_tag_name('div').find_element_by_tag_name('ul').find_elements_by_tag_name('li')
+    tab[-1].click()
+    table = driver.find_element_by_id('bill-roll-call-table').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+    try:
+        for tr in table:
+            r = tr.find_elements_by_tag_name('td')
+            vote_data_link = r[-1].find_element_by_tag_name('a').get_attribute('href')
+            soup = make_soup(vote_data_link)
+            vote_table = soup.find('dl', {'class': 'summary-table export-details'}).find_all('dd')
+            individual_votes = []
+            driver_two = open_driver(vote_data_link)
+            vote_details = driver_two.find_element_by_id('roll-call-detail-table').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+            for member in vote_details:
+                sleep(1)
+                member_details = member.find_elements_by_tag_name('td')
+                member_endpoint = member_details[0].find_element_by_tag_name('a').get_attribute('href')
+                soup_two = make_soup(member_endpoint)
+                name = " ".join(soup_two.find('h1').text.split()[1:])
+                member_vote = member_details[-1].text
+                hn = HumanName(name)
+                id = scraper_utils.legislators_search_startswith(column_val_to_return='goverlytics_id', column_to_search='name_first',
+                                                                 startswith=hn.first[0], name_last=hn.last)
+                individual_votes.append({'legislator': str(name), 'vote': str(member_vote), 'goverlytics_id': id})
+
+            votes.append({'date': vote_table[0].text, 'description': vote_table[1].text,
+                          'yea': str(vote_table[2].text), 'nay': vote_table[3].text,
+                          'absent': vote_table[4].text, 'passed': vote_table[5].text, 'nv': 0,
+                          'chamber': r[0].text, 'votes': individual_votes})
+    except NoSuchElementException:
+        print('NoSuchElementException ' + url)
+    except StaleElementReferenceException:
+        print('StaleElementReferenceException ' + url)
+    row.votes = votes
+    driver.quit()
+
+
+def get_bill_name(soup, row):
+    """
+    Get bill name.
+    :param soup: soup object
+    :param row: bill row
+    """
+    row.bill_name = soup.find('div', {'class': 'bill-title'}).find('h1').text
+
+
+@scraper_utils.Timer()
+def get_bill_committees(url, row):
+    committees = []
+    driver = open_driver(url)
+    driver.find_element_by_id('bills-tabs').find_elements_by_tag_name('li')[1].click()
     sleep(2)
-    tab = driver.find_element_by_class_name('nav nav-tabs bill-subtabs')
-    # tab.find_elements_by_tag_name('li')[-1].click()
-    # table = driver.find_element_by_id('bill-roll-call-table').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
-    # for tr in table:
-    #     row = tr.find_elements_by_tag_name('td')
-    #     vote_data_link = row[-1].find_element_by_tag_name('a').get_attribute('href')
-    #     soup = make_soup(BASE_URL + vote_data_link)
-    #     vote_table = soup.find('dl', {'class': 'summary-table export-details'}).find_all('dd')
-    #     individual_votes = []
-    #     vote_details = driver.find_element_by_id('roll-call-detail-table').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
-    #     for member in vote_details:
-    #         member_details = member.find_elements_by_tag_name('td')
-    #         member_endpoint = member_details[0].find_element_by_tag_name('a').get_attribute('href')
-    #         soup_two = make_soup(BASE_URL + '/' + member_endpoint)
-    #         name = " ".join(soup_two.find('h1').split()[1:])
-    #         member_vote = member_details[-1].text
-    #         individual_votes.append({'legislator': name, 'vote': member_vote})
-    #
-    #     votes.append({'date': vote_table[0].text, 'description': vote_table[1].text, 'yea': vote_table[2].text,
-    #                   'nay': vote_table[3].text, 'absent': vote_table[4].text, 'passed': vote_table[5].text, 'nv': 0,
-    #                   'chamber': row[0].text, 'votes': individual_votes})
-    #
-    # row.votes = votes
+    try:
+        committee_table = driver.find_element_by_id('meeting-history-table').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+        select = Select(driver.find_element_by_name('meeting-history-table_length'))
+        select.select_by_visible_text('All')
+        for tr in committee_table:
+            committee = tr.find_elements_by_tag_name('td')[-1]
+            committee_dict = {'chamber': committee.text.split()[0], 'committee': committee.text}
+            if committee_dict not in committees:
+                committees.append(committee_dict)
+        row.committees = committees
+    except NoSuchElementException:
+        print('NoSuchElementException ' + url)
+    except StaleElementReferenceException:
+        print('STALE ' + url)
+    driver.quit()
+
+
+def get_bill_type(url, row):
+    info = url.split('/')
+    bill_name = info[-1]
+    type = info[1]
+    house_or_senate = 'House' if bill_name == 'H' else 'Senate'
+    row.bill_type = f'{house_or_senate} {type.title()}'
 
 
 def scrape(url):
@@ -246,19 +305,19 @@ def scrape(url):
     row.source_url = url
 
     get_goverlytics_id(soup, row)
-    # get_bill_text(soup, row)
-    get_bill_sponsor_and_committees(soup, row)
-    get_session(soup, row)
-    # get_bill_actions(url, row)
+    get_bill_text(soup, row)
+    get_bill_sponsor(soup, row)
+    get_bill_session(soup, row)
+    get_bill_actions(url, row)
     get_bill_votes(url, row)
-    # get_bill_name(url, row)
+    get_bill_name(soup, row)
     # get_date_introduced(url, row)
-    # get_bill_type(url, row)
-    # get_committees(url, row)
+    get_bill_type(url, row)
+    get_bill_committees(url, row)
     get_current_status(soup, row)
     get_bill_summary(soup, row)
 
-    pprint(row)
+    # pprint(row)
     return row
 
 
@@ -270,8 +329,7 @@ def main():
     with Pool() as pool:
         data = pool.map(scrape, all_urls)
 
-    # data = [scrape(url) for url in urls]
-    # scraper_utils.write_data(data, 'us_nm_legislation')
+    scraper_utils.write_data(data, 'us_vt_legislation')
 
 
 if __name__ == '__main__':
