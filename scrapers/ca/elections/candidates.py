9f51e492f96776891af822abc2eb33533640450d
Hyunjin Nha <hyunjinnha@gmail.com>
2021-08-05 11:42:46 -0700

    Added image urls to df rather than going through the list another time.

--- scrapers/ca/elections/candidates.py
+++ scrapers/ca/elections/candidates.py
@@ -33,11 +33,9 @@ def program_driver():
     print("Collecting data...")
     scraper = Scraper()
     candidate_table_df = scraper.get_data()
-    images_data = scraper.get_images()
-    print(images_data)
 
     data_organizer = Organizer()
-    data_organizer.set_rows(candidate_table_df, images_data)
+    data_organizer.set_rows(candidate_table_df)
     rows = data_organizer.get_rows()
     # scraper_utils.write_data(rows)
     
@@ -49,7 +47,7 @@ class Scraper:
         self.driver.start_driver(CANDIDATES_URL, crawl_delay)
 
         try:
-            self.data, self.images = self._get_candidate_data()
+            self.data = self._get_candidate_data()
         except Exception as e:
             print(e.with_traceback())
 
@@ -58,38 +56,33 @@ class Scraper:
     def get_data(self):
         return self.data
 
-    def get_images(self):
-        return self.images
-
     def _get_candidate_data(self):
         self._prepare_page_for_collection()
         return self._collect_data()
     
     def _prepare_page_for_collection(self):
         self._expand_all_entries()
-        self._view_1000_entries()
+        # self._view_1000_entries()
 
     def _expand_all_entries(self):
         expand_all_button = self.driver.driver.find_element_by_css_selector('#gridContainer > div > div.dx-datagrid-header-panel > div > div > div.dx-toolbar-after > div:nth-child(2) > div > div > div')
         expand_all_button.click()
         sleep(10)
 
-    def _view_1000_entries(self):
-        view_1000_div = self.driver.driver.find_elements_by_class_name('dx-page-size')[-1]
-        view_1000_div.click()
-        sleep(20)
-        self.driver.driver.find_element_by_tag_name('html').send_keys('Keys.END')
+    # def _view_1000_entries(self):
+    #     view_1000_div = self.driver.driver.find_elements_by_class_name('dx-page-size')[-1]
+    #     view_1000_div.click()
+    #     sleep(20)
+    #     self.driver.driver.find_element_by_tag_name('html').send_keys('Keys.END')
 
     def _collect_data(self):
         data = DataFrame()
-        images = []
         # while self._get_next_page_button():
-        for i in range(0,2):
+        for i in range(0,3):
             data = data.append(self._get_page_as_df())
-            images.extend(self._get_candidate_images())
             self._get_next_page_button().click()
-            sleep(5)
-        return data, images
+            sleep(1)
+        return data
 
     def _get_next_page_button(self):
         next_page_button = self.driver.driver.find_elements_by_class_name('dx-navigate-button')[-1]
@@ -102,13 +95,13 @@ class Scraper:
         html_table_data = html_soup.find_all('table', {'class': 'dx-datagrid-table dx-datagrid-table-fixed'})[1]
         df = pd.read_html(str(html_table_data), index_col=[0, 1, 2, 3])[0]
         df.columns = ['Province or Territory', 'Constituency', 'Candidate', 'Gender', 'Occupation', 'Political Affiliation', 'Result', 'Votes']
+        df['Image URL'] = None
+        self._get_candidate_images(html_soup, df)
+
         return df
 
-    def _get_candidate_images(self):
-        page_html = self.driver.get_html_source()
-        html_soup = soup(page_html, 'html.parser')
+    def _get_candidate_images(self, html_soup: soup, df):
         trs_with_images = html_soup.find_all('tr', {'class':'dx-row dx-data-row dx-column-lines'})
-        images_names = []
         for tr in trs_with_images:
             name = tr.find_all('td')[6].text
 
@@ -123,27 +116,20 @@ class Scraper:
                 candidate_url = CANDIDATES_BASE_URL + candidate_url
                 img_url = self._get_img_url_from_candidate_profile(candidate_url)
 
-            images_names.append(
-                {
-                    'name': name,
-                    'image_url': img_url,
-                    'profile_url': candidate_url
-                }
-            )
+            df.loc[df["Candidate"] == name, ['Image URL']] = img_url
 
-        return images_names
-    
     def _get_img_url_from_candidate_profile(self, url):
         self.driver.driver.execute_script(f'''window.open("{url}", "_blank");''')
-        sleep(2)
+        sleep(3.5)
         self.driver.tabs += 1
         self.driver.driver.switch_to_window(self.driver.driver.window_handles[self.driver.tabs - 1])
         image_div = self.driver.driver.find_element_by_id('PersonPic')
-        img_url = CANDIDATES_BASE_URL + image_div.find_element_by_tag_name('img').get_attribute('src')
+        img_url = image_div.find_element_by_tag_name('img').get_attribute('src')
         self.driver.driver.close()
+        sleep(0.5)
         self.driver.tabs -= 1
         self.driver.driver.switch_to_window(self.driver.driver.window_handles[self.driver.tabs - 1])
-        sleep(2)
+        sleep(1)
         return img_url
 
 class SeleniumDriver:
@@ -181,20 +167,20 @@ class Organizer:
     def __init__(self):
         self.rows = []
 
-    def set_rows(self, df: DataFrame, images_data):
+    def set_rows(self, df: DataFrame):
         for index, row in df.iterrows():
             value = row['Province or Territory']
             date_of_election = None
             if not ((pd.isna(value)) or 'Continued from'in value or 'Continues on' in value or 'Date of Election' in value or 
                         'Type of Election' in value or 'Parliament' in value):
-                self._add_row_data(row, images_data, date_of_election)
+                self._add_row_data(row, date_of_election)
             elif pd.notna(value) and 'Date of Election' in value:
                 date_of_election = re.search(r'[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}', value).group()
 
     def get_rows(self):
         return self.rows
 
-    def _add_row_data(self, data_row, images_data, election_date):
+    def _add_row_data(self, data_row, election_date):
         row = scraper_utils.initialize_row()
         row.current_electoral_district_id = self._get_district_id(data_row)
         self._set_name_data(row, data_row)
--- scrapers/ca/elections/candidates_test.py
+++ scrapers/ca/elections/candidates_test.py
@@ -7,13 +7,27 @@ import numpy as np
 
 pd.get_option("display.max_columns")
 options = Options()
-# options.headless = True
+options.headless = True
 driver = webdriver.Chrome('web_drivers/chrome_win_92.0.4515.43/chromedriver.exe', options=options)
 driver.switch_to.default_content()
 
 driver.get('https://lop.parl.ca/sites/ParlInfo/default/en_CA/ElectionsRidings/Elections')
 driver.maximize_window()
 sleep(10)
+
+def get_img_url_from_candidate_profile(url):
+    driver.execute_script(f'''window.open("{url}", "_blank");''')
+    sleep(4)
+    tabs = 2
+    driver.switch_to_window(driver.window_handles[tabs - 1])
+    image_div = driver.find_element_by_id('PersonPic')
+    img_url = image_div.find_element_by_tag_name('img').get_attribute('src')
+    driver.close()
+    sleep(1)
+    tabs -= 1
+    driver.switch_to_window(driver.window_handles[tabs - 1])
+    sleep(2)
+    return img_url
 
 expand_all = driver.find_element_by_css_selector('#gridContainer > div > div.dx-datagrid-header-panel > div > div > div.dx-toolbar-after > div:nth-child(2) > div > div > div')
 expand_all.click()
@@ -25,6 +39,25 @@ html_data = html_soup.find_all('table', {'class': 'dx-datagrid-table dx-datagrid
 df = pd.read_html(str(html_data), index_col=[0, 1, 2, 3])[0]
 print(df.columns)
 df.columns = ['Province or Territory', 'Constituency', 'Candidate', 'Gender', 'Occupation', 'Political Affiliation', 'Result', 'Votes']
+df['Image URL'] = None
+
+trs_with_images = html_soup.find_all('tr', {'class':'dx-row dx-data-row dx-column-lines'})
+
+for tr in trs_with_images:
+    name = tr.find_all('td')[6].text
+
+    img_url = tr.find('img')
+    if img_url is not None:
+        img_url = img_url.get('src')
+        img_url = 'https://lop.parl.ca' + img_url
+
+    candidate_url = tr.find_all('td')[6].a
+    if candidate_url is not None:
+        candidate_url = candidate_url['href']
+        candidate_url = 'https://lop.parl.ca' + candidate_url
+        img_url = get_img_url_from_candidate_profile(candidate_url)
+    df.loc[df["Candidate"] == name, ['Image URL']] = img_url
+
+
 driver.close()
-print(df)
 df.to_csv('data.txt', sep='\t')
