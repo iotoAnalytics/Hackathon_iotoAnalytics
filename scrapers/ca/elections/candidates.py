2f517e79201c27fe05a396a9d526431e6ab3b982
Hyunjin Nha <hyunjinnha@gmail.com>
2021-08-10 13:52:59 -0700

    Added documentation and changed some of the code structure.

--- scrapers/ca/elections/candidates.py
+++ scrapers/ca/elections/candidates.py
@@ -1,12 +1,29 @@
-# TODO: Write documentation for code.
+"""
+This program is used for scraping all the candidates that ran in a Canadian Federal Election from 1867.
+It was run to scrape data on 2021-08-10, and it should not be necessary to run again afterwards.
+The current version adds candidates based looking at all the Canadian Legislators in the database currently,
+and if it doesn't exists, assigns a new goverlytics_id. 
+Seriously, there are around 45000 data entries all using Selenium, so it takes a long time.
+
+For version 2.0
+-------------------
+Version 2.0 should be made for subsequent elections so that candidates are checked against the ca_candidates table.
+If a candidate already exists in the table, it would need to update the candidate if party or district has changed.
+If a candidate does not exist in the table, a new goverlytics_id using the legislator_id sequence in the db should be used.
+
+Details
+----------
+Author: Kevin Nha
+Version: 1.0
+Date: 2021-08-10
+"""
 
 import os
 from pathlib import Path
 import re
 import sys
 from time import sleep
-
-from pandas.core.frame import DataFrame
+from typing import Union
 
 NODES_TO_ROOT = 3
 path_to_root = Path(os.path.abspath(__file__)).parents[NODES_TO_ROOT]
@@ -16,6 +33,7 @@ from bs4 import BeautifulSoup as soup
 from nameparser import HumanName
 from numpy import nan
 import pandas as pd
+from pandas.core.frame import DataFrame
 from scraper_utils import CandidatesScraperUtils
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -43,6 +61,9 @@ def program_driver():
     print("complete")
 
 class Scraper:
+    """
+    Used to scrape data from the website.
+    """
     def __init__(self):
         self.driver = SeleniumDriver()
         self.driver.start_driver(CANDIDATES_URL, crawl_delay)
@@ -63,23 +84,15 @@ class Scraper:
     
     def _prepare_page_for_collection(self):
         self._expand_all_entries()
-        # self._view_1000_entries()
 
     def _expand_all_entries(self):
         expand_all_button = self.driver.driver.find_element_by_css_selector('#gridContainer > div > div.dx-datagrid-header-panel > div > div > div.dx-toolbar-after > div:nth-child(2) > div > div > div')
         expand_all_button.click()
         sleep(10)
 
-    # def _view_1000_entries(self):
-    #     view_1000_div = self.driver.driver.find_elements_by_class_name('dx-page-size')[-1]
-    #     view_1000_div.click()
-    #     sleep(20)
-    #     self.driver.driver.find_element_by_tag_name('html').send_keys('Keys.END')
-
     def _collect_data(self):
         data = DataFrame()
         while self._get_next_page_button():
-        # for i in range(0,1):
             data = data.append(self._get_page_as_df())
             self._get_next_page_button().click()
             sleep(1)
@@ -121,19 +134,31 @@ class Scraper:
 
     def _get_img_url_from_candidate_profile(self, url):
         self.driver.driver.execute_script(f'''window.open("{url}", "_blank");''')
-        sleep(3.5)
         self.driver.tabs += 1
         self.driver.driver.switch_to_window(self.driver.driver.window_handles[self.driver.tabs - 1])
-        image_div = self.driver.driver.find_element_by_id('PersonPic')
-        img_url = image_div.find_element_by_tag_name('img').get_attribute('src')
+
+        get_image_try_count = 5
+        while get_image_try_count > 0:
+            try:
+                image_div = self.driver.driver.find_element_by_id('PersonPic')
+                img_url = image_div.find_element_by_tag_name('img').get_attribute('src')
+                break
+            except:
+                sleep(2)
+                get_image_try_count -= 1
+        if get_image_try_count == 0:
+            img_url = ''
         self.driver.driver.close()
-        sleep(0.5)
+        sleep(1.5)
         self.driver.tabs -= 1
         self.driver.driver.switch_to_window(self.driver.driver.window_handles[self.driver.tabs - 1])
         sleep(1)
         return img_url
 
 class SeleniumDriver:
+    """
+    Used to handle Selenium.
+    """
     def __init__(self):
         self.driver = webdriver.Chrome('web_drivers/chrome_win_92.0.4515.43/chromedriver.exe', options=options)
         self.driver.switch_to.default_content()
@@ -165,6 +190,10 @@ class SeleniumDriver:
             raise RuntimeError("Error in getting email table from selenium.")
 
 class Organizer:
+    """
+    Used to organize the data collected by the Scraper.
+    Initializes and sets the row data. 
+    """
     def __init__(self):
         self.rows = []
         self.new_entries = []
@@ -260,6 +289,15 @@ class Organizer:
         return name_first + ' ' + name_last
 
     def get_goverlytics_id(self, row, election_date):
+        """
+        Gets the goverlytics_id of existing candidates.
+
+        Returns
+        -----------
+        None if candidate has been already checked\n
+        -10 if candidate doesn't exist in the ca_legislators table\n
+        goverlytics_id (int) of corresponding legislator in ca_legislators table
+        """
         name_full = self._get_full_name(row)
         
         if name_full in self.checked_list and self._is_candidate_checked(row):
@@ -268,14 +306,24 @@ class Organizer:
         name_match_df = self._get_legislator_name_match_df(row)
 
         if name_match_df.empty:
-            return self._add_new_candidate_to_checked_list(row, election_date)
+            self._add_new_candidate_to_checked_list(row, election_date)
+            return -10
 
         if len(name_match_df) > 1:
             return self._handle_multiple_instances_of_candidate(row, name_match_df, election_date)
 
         return self._handle_single_instance_of_candidate(row, name_match_df, election_date)
 
-    def _is_candidate_checked(self, row):
+    def _is_candidate_checked(self, row) -> bool:
+        """
+        Checks whether a candidate has already been checked and a row containing data for that candidate is already initialized.
+
+        Returns
+        ---------
+        True: if candidate has been checked already\n
+        False: if candidate has not been checked
+        """
+
         print("This candidate seems to have been added already.")
         Printer().print_candidate_info(row)
         print(f"======================\n" +
@@ -299,35 +347,62 @@ class Organizer:
                 print("Please enter y or n")
 
     def _get_legislator_name_match_df(self, row) -> DataFrame:
+        """
+        Searches the ca_legislator table to find legislators that have the same last_name and first_name.
+
+        Returns
+        --------
+        DataFrame: DataFrame of ca_legislator table where name matches
+        """
         name_last = row.name_last
         name_first = row.name_first
         name_last_match = self.legislators_df["name_last"].apply(str.lower) == name_last
         name_first_match = self.legislators_df["name_first"].apply(str.lower) == name_first
         return self.legislators_df.loc[(name_last_match) & (name_first_match)]
 
-    def _handle_multiple_instances_of_candidate(self, row, name_match_df: DataFrame, election_date):
+    def _handle_multiple_instances_of_candidate(self, row, name_match_df: DataFrame, election_date) -> int:
+        """
+        Handles case where there are multiple legislators with the same name as a candidate in the ca_legislators table.
+
+        Returns
+        ---------
+        -10 if candidate with the same name in ca_legislator table are not the same person\n
+        goverlytics_id (int) of legislator that is the same person as the candidate
+        """
         Printer().print_candidate_info(row)
         print("More than one instance of the candidate with the same name was found in the database:")
         Printer().print_candidates_from_df(name_match_df)
         user_input = None
         while user_input is None:
             user_input_gov_id = input("Enter the goverlytic_id of correct candidate. If none, please enter 'None': ")
-            user_input = self._get_user_input_for_multiple_candidate_instances(user_input_gov_id, name_match_df)
-        if user_input == "EXISTS":
-            return self._add_existing_candidate_to_checked_list(name_match_df, int(user_input_gov_id), election_date)
-        elif user_input == "NEW":
-            return self._add_new_candidate_to_checked_list(row, election_date)
+            user_input = self._candidate_exists_in_ca_legislator_table(user_input_gov_id, name_match_df)
+        if user_input == True:
+            self._add_existing_candidate_to_checked_list(name_match_df, int(user_input_gov_id), election_date)
+            return int(user_input_gov_id)
+        elif user_input == False:
+            self._add_new_candidate_to_checked_list(row, election_date)
+            return -10
 
-    def _get_user_input_for_multiple_candidate_instances(self, user_input_gov_id, name_match_df: DataFrame):
+    def _candidate_exists_in_ca_legislator_table(self, user_input_gov_id, name_match_df: DataFrame) -> Union[bool, None]:
+        """
+        Handles the manual input of candidate check with legislator when name is the same but other attributes are different.
+
+        Returns
+        --------
+        True: if user enters 'y' and user entered the correct gov_id for user_input_gov_id
+        False: if user enters 'n' and user entered 'None' for user_input_gov_id
+        None: if user made a mistake in selecting the user_input_gov_id
+        
+        """
         possible_gov_ids = name_match_df['goverlytics_id'].values
 
         if user_input_gov_id == "None" or (user_input_gov_id.isnumeric() and int(user_input_gov_id) in possible_gov_ids):
             while True:
                 user_input = input(f"You entered: {user_input_gov_id}. Is this correct? (y/n): ")
                 if user_input == 'y' and user_input_gov_id != "None":
-                    return "EXISTS"
+                    return True
                 elif user_input == 'y' and user_input_gov_id == "None":
-                    return "NEW"
+                    return False
                 elif user_input == 'n':
                     print("Try again.")
                     return
@@ -337,6 +412,15 @@ class Organizer:
             print("Make sure your input matches one of the goveryltic_ids or is None.")
 
     def _handle_single_instance_of_candidate(self, row, name_match_df: DataFrame, election_date):
+        """
+        Handles the case where a single legisatlor exists with the same name as the candidate.
+        Requires a manual user input for confirmation if there is a difference in the party or electoral_district.
+
+        Returns
+        ---------
+        -10: if candidate with the same name in ca_legislator table are not the same person
+        goverlytics_id (int): if legislator is the same person as the candidate
+        """
         row_party_id = row.current_party_id
         match_party_id = name_match_df['party_id'].values[0]
         if int(match_party_id) != int(row_party_id):
@@ -350,9 +434,19 @@ class Organizer:
             return self._get_user_input_for_candidate_info_mismatch("'district'", row, name_match_df, election_date)
 
         gov_id = name_match_df['goverlytics_id'].values[0]
-        return self._add_existing_candidate_to_checked_list(name_match_df, gov_id, election_date)
+        self._add_existing_candidate_to_checked_list(name_match_df, int(gov_id), election_date)
+        return int(gov_id)
 
     def _get_user_input_for_candidate_info_mismatch(self, mismatch_column, row, name_match_df, election_date):
+        """
+        Prints an informative message for what attribute is mismatched.
+        It then requires user input to determine whether the legislator and candidate are the same individual
+
+        Returns
+        ---------
+        -10: if candidate with the same name in ca_legislator table are not the same person
+        goverlytics_id (int): if legislator is the same person as the candidate
+        """
         print(f"Candidate may already exist in the database, but the {mismatch_column} does not match.")
         print(f"Please check if candidate [{row.name_full}] is the same based on the information below:")
         Printer().print_candidate_info(row)
@@ -362,23 +456,15 @@ class Organizer:
             user_input = input("Are these the same individual? y/n: ")
             if user_input == 'y':
                 gov_id =  name_match_df['goverlytics_id'].values[0]
-                return self._add_existing_candidate_to_checked_list(name_match_df, gov_id, election_date)
+                self._add_existing_candidate_to_checked_list(name_match_df, int(gov_id), election_date)
+                return int(gov_id)
             elif user_input == 'n':
-                return self._add_new_candidate_to_checked_list(row, election_date)
+                self._add_new_candidate_to_checked_list(row, election_date)
+                return -10
             else:
                 print("Please enter y or n")
 
-    def _append_to_checked_list(self, name_full, gov_id, party, district, election_date):
-        self.checked_list.setdefault(name_full, [])
-        to_add = {
-            'goverlytic_id': gov_id,
-            'party': party,
-            'electoral_district': district,
-            'most_recent_election_date': election_date
-        }
-        self.checked_list.get(name_full).append(to_add)
-
-    def _add_new_candidate_to_checked_list(self, row, election_date):
+    def _add_new_candidate_to_checked_list(self, row, election_date) -> None:
         name_full = self._get_full_name(row)
         ed_id = row.current_electoral_district_id
         party_id = row.current_party_id
@@ -387,9 +473,8 @@ class Organizer:
         ed = self.value_getter.get_value(self.ed_df, 'id', ed_id, 'district_name')
 
         self._append_to_checked_list(name_full, None, party, ed, election_date)
-        return -10
 
-    def _add_existing_candidate_to_checked_list(self, name_match_df: DataFrame, gov_id, election_date):
+    def _add_existing_candidate_to_checked_list(self, name_match_df: DataFrame, gov_id, election_date) -> None:
         match_row = name_match_df.loc[name_match_df['goverlytics_id'] == gov_id]
 
         name_last = match_row['name_last'].values[0]
@@ -401,7 +486,19 @@ class Organizer:
         name_match_df_riding = name_match_df_riding.replace('—', '--')
 
         self._append_to_checked_list(name_full, gov_id, name_match_df_party, name_match_df_riding, election_date)
-        return int(gov_id)
+
+    def _append_to_checked_list(self, name_full, gov_id, party, district, election_date) -> None:
+        """
+        Appends candidate to the checked list.
+        """
+        self.checked_list.setdefault(name_full, [])
+        to_add = {
+            'goverlytic_id': gov_id,
+            'party': party,
+            'electoral_district': district,
+            'most_recent_election_date': election_date
+        }
+        self.checked_list.get(name_full).append(to_add)
 
 class DataValue:
     def get_value(self, df: DataFrame, search_column, search_value, return_column):
