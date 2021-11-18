3c1214f0ffd8baee6d2bc05a6728643f3e83ddef
Kevin <60684083+KevinNha@users.noreply.github.com>
2021-11-18 14:38:40 -0800

    Update ca_nl_legislator_scraper.py

--- scrapers/ca/ca-provinces/NL/legislators/ca_nl_legislator_scraper.py
+++ scrapers/ca/ca-provinces/NL/legislators/ca_nl_legislator_scraper.py
@@ -45,9 +45,9 @@ crawl_delay = scraper_utils.get_crawl_delay(BASE_URL)
 
 def scrape(url):
     options = Options()
-    options.headless = True
-    options.add_argument('--User-Agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/79.0.3945.88 Safari/537.36; IOTO International Inc./enquiries@ioto.ca')
-    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36; IOTO International Inc./enquiries@ioto.ca')
     
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.switch_to.default_content()
