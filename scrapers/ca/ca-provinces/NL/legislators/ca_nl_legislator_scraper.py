91937ffc1215128358f617c483c1ddefe8a33b8d
Hyunjin Nha <hyunjinnha@gmail.com>
2021-11-18 14:48:37 -0800

    Trying to fix headless driver.

--- scrapers/ca/ca-provinces/NL/legislators/ca_nl_legislator_scraper.py
+++ scrapers/ca/ca-provinces/NL/legislators/ca_nl_legislator_scraper.py
@@ -46,8 +46,7 @@ crawl_delay = scraper_utils.get_crawl_delay(BASE_URL)
 def scrape(url):
     options = Options()
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36; IOTO International Inc./enquiries@ioto.ca')
+    options.add_argument('--User-Agent=IOTO International Inc./enquiries@ioto.ca')
     
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.switch_to.default_content()
