faf0b023c5a244380144433485565d621c8bd802
Hyunjin Nha <hyunjinnha@gmail.com>
2021-12-07 18:11:08 -0800

    Slightly changed template file.

--- templates/template_minimal/legislators/legislator_template.py
+++ templates/template_minimal/legislators/legislator_template.py
@@ -2,26 +2,19 @@
 This template is meant to serve as a general outline, and will not necessarily work for
 all collectors. Feel free to modify the script as necessary.
 '''
-import sys
 import os
+import sys
 from pathlib import Path
 
 # Get path to the root directory so we can import necessary modules
-p = Path(os.path.abspath(__file__)).parents[5]
-
+NODES_TO_ROOT = 5
+p = Path(os.path.abspath(__file__)).parents[NODES_TO_ROOT]
 sys.path.insert(0, str(p))
 
-from scraper_utils import USStateLegislatorScraperUtils
-from bs4 import BeautifulSoup
-import requests
+from bs4 import BeautifulSoup as soup
+from scraper_utils import USStateLegislatorScraperUtils # This tempalte is for an American State
 from multiprocessing import Pool
-from database import Database
-from pprint import pprint
 from nameparser import HumanName
-import re
-import boto3
-import time
-
 
 state_abbreviation = 'AZ'
 database_table_name = 'legislator_template_test'
@@ -33,7 +26,6 @@ base_url = 'https://webscraper.io'
 # Get the crawl delay specified in the website's robots.txt file
 crawl_delay = scraper_utils.get_crawl_delay(base_url)
 
-
 def get_urls():
     '''
     Insert logic here to get all URLs you will need to scrape from the page.
@@ -44,19 +36,20 @@ def get_urls():
     path = '/test-sites/e-commerce/allinone'
     scrape_url = base_url + path
     page = scraper_utils.request(scrape_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    page_soup = soup(page.content, 'html.parser')
     urls = [base_url + prod_path['href']
-            for prod_path in soup.findAll('a', {'class': 'title'})]
+            for prod_path in page_soup.findAll('a', {'class': 'title'})]
 
     # Delay so we don't overburden web servers
     scraper_utils.crawl_delay(crawl_delay)
 
     return urls
 
-
 def scrape(url):
     # Send request to website
     page = scraper_utils.request(url)
+    page_soup = soup(page.content, 'html.parser')
+
     row = scraper_utils.initialize_row()
 
     # ... Collect data from page
@@ -65,7 +58,6 @@ def scrape(url):
     scraper_utils.crawl_delay(crawl_delay)
 
     return row
-
 
 if __name__ == '__main__':
     # First we'll get the URLs we wish to scrape:
